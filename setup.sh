ae12c8c0a9d627d74ccc3696ed02f34a7d07f387
lpil <lpil@contact.me.via.github.fake>
2014-02-02 22:51:21 +0000

    setup.sh fix

--- setup.sh
+++ setup.sh
@@ -4,7 +4,15 @@ if [ ~/.vimrc ]; then
   echo Backing up existing .vimrc to .vimrc.bak
   mv -iv ~/.vimrc ~/.vimrc.bak
 fi
-ln -sv $(dirname $0)/.vimrc ~/.vimrc
+# Get full path for this directory
+VIMRCPATH="$( cd "$(dirname "$0")" ; pwd -P )"
+
+# Make the symlink
+ln -sv "$VIMRCPATH/.vimrc" ~/.vimrc
+
+# Get the plugin package manager, vundle
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+
+# Install plugins
 vim +BundleInstall +qall
-echo Done...!
+echo "\033[0;32mDone...!\033[0m"
