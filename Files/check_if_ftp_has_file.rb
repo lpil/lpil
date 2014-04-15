657d9780e612cd14648339a4bcaf7468d3621321
Louis <lpil@users.noreply.github.com>
2014-04-15 12:51:56 +0100

    ftp has file - style fix

--- Files/check_if_ftp_has_file.rb
+++ Files/check_if_ftp_has_file.rb
@@ -4,7 +4,7 @@
 require 'net/ftp'
 
 def ftp_has_file?(ftp_addr, ftp_user, ftp_pass, filename)
-  Net::FTP.open(ftp_addr, ftp_user, ftp_pass) { |ftp|
+  Net::FTP.open(ftp_addr, ftp_user, ftp_pass) do |ftp|
     return ftp.nlst.include? filename
-  }
+  end
 end
