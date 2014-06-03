2f6b64c5df8beed03139f169a462647d748c46f3
Louis <louis@lpil.co.uk>
2014-06-03 15:43:27 +0100

    RESTful delivery routes

--- routes.rb
+++ routes.rb
@@ -8,14 +8,14 @@ end
 # DPD + post delivery tracking
 #
 
-get '/deliveries' do
-  slim :deliveries_form
-end
-
-post '/deliveries' do
-  @dpd_url = 'http://www.dpd.co.uk/apps/tracking/?reference='
-  @mailing = Mailing.find_by_order_ref(params[:order_ref])
-  slim :deliveries_result
+get '/delivery' do
+  if params['order_ref']
+    @dpd_url = 'http://www.dpd.co.uk/apps/tracking/?reference='
+    @mailing = Mailing.find_by_order_ref(params[:order_ref])
+    slim :deliveries_result
+  else
+    slim :deliveries_form
+  end
 end
 
 #
