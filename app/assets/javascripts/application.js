b39641a83f53df72a746d478d6d457c68649a09a
Louis Pilfold <louis@lpil.uk>
2014-07-24 14:43:15 +0100

    No turbolinks, responsive layout

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,7 +13,7 @@
 //= require jquery
 //= require jquery_ujs
 //= require foundation
-//= require turbolinks
+// no= require turbolinks
 //= require_tree .
 
 $(function(){ $(document).foundation(); });
