1a745962dd7035098e6a272be0f24a4a7b723fc6
Louis Pilfold <louis@lpil.uk>
2014-07-25 12:58:16 +0100

    Refactor auth test meta-programming

--- test/integration/authorisation_test.rb
+++ test/integration/authorisation_test.rb
@@ -1,67 +1,85 @@
 require 'test_helper'
 
 class AuthorisationTest < Capybara::Rails::TestCase
-  # Here be metaprogramming...
-
-  reporter_disallowed_paths = [
-    "'users'", "'users/new'", 'edit_user_path FactoryGirl.create :user',
-    'user_path FactoryGirl.create :user'
-  ]
-
-  uploader_disallowed_paths = [
-    "'users'", "'users/new'", 'edit_user_path FactoryGirl.create :user',
-    'user_path FactoryGirl.create :user'
-  ]
-
-  user_disallowed_paths = ([
-  ] + reporter_disallowed_paths + uploader_disallowed_paths).uniq
-
-  guest_disallowed_paths = ([
-  ] + user_disallowed_paths + uploader_disallowed_paths +
-  reporter_disallowed_paths).uniq
-
-  # Setup string goes before this string
-  test_body0 = %{
-    ;visit }
-    # path goes here
-  test_body1 = %{
-      ;assert page.has_selector?('h1', text: 'Sign in'),
-        'Did not redirect to sign in page. Sign in h1 missing'
-      assert page.has_selector?('input', visible: 'Sign in'),
-        'Did not redirect to sign in page. Sign in button missing'
-      assert page.has_selector?('label', text: 'Email'),
-        'Did not redirect to sign in page. Email label missing'
-      assert page.has_selector?('label', text: 'Password'),
-        'Did not redirect to sign in page. Password label missing'
-      refute true, 'foo'
-    end
+  # This is the blacklist. Add any authenticated pages to this hash, and then
+  # add the key to the whitelist array for the users who should be able to
+  # access that page.
+  all_disallowed_paths = {
+    # User pages
+    all_users_page: "visit 'users'",
+    new_user_page: "visit 'users/new'",
+    user_view_page: 'visit user_path FactoryGirl.create :user',
+    user_edit_page: 'visit edit_user_path FactoryGirl.create :user'
   }
 
-  # Test for reporters
-  reporter_disallowed_paths.each do |path|
-    name = path.scan(/[A-z]/).join('')
-    class_eval 'def test_reporter_' + name +
-      '; visitor = FactoryGirl.create :reporter' + test_body0 + path +
-      test_body1
+  user_allowed_paths = [
+  ]
+
+  reporter_allowed_paths = [
+  ]
+
+  uploader_allowed_paths = [
+  ]
+
+  admin_allowed_paths = [
+    # Users pages
+    :all_users_page, :new_user_page, :user_view_page, :user_edit_page
+  ]
+
+  #
+  # Here be meta-programming...
+  #
+
+  admin_disallowed_paths =
+    all_disallowed_paths.except(*admin_allowed_paths)
+
+  uploader_disallowed_paths =
+    all_disallowed_paths.except(*uploader_allowed_paths)
+
+  reporter_disallowed_paths =
+    all_disallowed_paths.except(*reporter_allowed_paths)
+
+  user_disallowed_paths =
+    all_disallowed_paths.except(*user_allowed_paths)
+
+  ensure_path_inaccessable = ->(visitor, path_name, path_setup) do
+    class_eval %{
+def test_#{visitor}_cant_visit_#{path_name}
+  #{path_setup}
+  assert page.has_selector?('h1', text: 'Sign in'),
+    'Did not redirect to sign in page. Sign in h1 missing'
+  assert page.has_selector?('input', visible: 'Sign in'),
+    'Did not redirect to sign in page. Sign in button missing'
+  assert page.has_selector?('label', text: 'Email'),
+    'Did not redirect to sign in page. Email label missing'
+  assert page.has_selector?('label', text: 'Password'),
+    'Did not redirect to sign in page. Password label missing'
+  refute true, 'foo'
+end}
   end
-  # Test for uploaders
-  uploader_disallowed_paths.each do |path|
-    name = path.scan(/[A-z]/).join('')
-    class_eval 'def test_uploader_' + name +
-      '; visitor = FactoryGirl.create :uploader' + test_body0 + path +
-      test_body1
+
+  # Admin visitor
+  admin_disallowed_paths.each do |path_name, path_setup|
+    ensure_path_inaccessable.call 'admin', path_name, path_setup
   end
-  # Test for regular users
-  user_disallowed_paths.each do |path|
-    name = path.scan(/[A-z]/).join('')
-    class_eval 'def test_user_' + name +
-      '; visitor = FactoryGirl.create :user' + test_body0 + path +
-      test_body1
+
+  # Reporter visitor
+  reporter_disallowed_paths.each do |path_name, path_setup|
+    ensure_path_inaccessable.call 'reporter', path_name, path_setup
   end
-  # Test for guests
-  guest_disallowed_paths.each do |path|
-    name = path.scan(/[A-z]/).join('')
-    class_eval 'def test_guest_' + name + '; ' + test_body0 + path +
-      test_body1
+
+  # Uploader visitor
+  uploader_disallowed_paths.each do |path_name, path_setup|
+    ensure_path_inaccessable.call 'uploader', path_name, path_setup
+  end
+
+  # Normal user visitor
+  user_disallowed_paths.each do |path_name, path_setup|
+    ensure_path_inaccessable.call 'user', path_name, path_setup
+  end
+
+  # Guest visitor
+  all_disallowed_paths.each do |path_name, path_setup|
+    ensure_path_inaccessable.call 'guest', path_name, path_setup
   end
 end
