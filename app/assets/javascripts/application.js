f820b3b1d4bd8ff61e58306b82d32eaa63d79492
Louis Pilfold <louis@lpil.uk>
2014-07-26 01:34:03 +0100

    slim foundation js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,8 +14,27 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require foundation
-// no= require turbolinks
 //= require_tree .
+
+//= require foundation/foundation
+//= require foundation/foundation.topbar
+
+// no= require foundation/foundation.abide
+// no= require foundation/foundation.accordion
+// no= require foundation/foundation.alert
+// no= require foundation/foundation.clearing
+// no= require foundation/foundation.dropdown
+// no= require foundation/foundation.equalizer
+// no= require foundation/foundation.interchange
+// no= require foundation/foundation.joyride
+// no= require foundation/foundation.magellan
+// no= require foundation/foundation.offcanvas
+// no= require foundation/foundation.orbit
+// no= require foundation/foundation.reveal
+// no= require foundation/foundation.slider
+// no= require foundation/foundation.tab
+// no= require foundation/foundation.tooltip
+
+// no= require turbolinks
 
 $(function(){ $(document).foundation(); });
