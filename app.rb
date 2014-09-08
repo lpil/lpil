347765c1a882180e479957a7f882f4148792fefc
Louis Pilfold <louis@lpil.uk>
2014-09-08 13:51:43 +0100

    Remove redundant app.rb file

--- app.rb
+++
@@ -1,8 +0,0 @@
-#
-# Periodic Tasks
-#
-
-#  Delete mailings more than 90 days old from the database
-$threads[:delete_old_mailings] = every 1.hour do
-  Mailing.delete_all(['date_sent < ?', 90.days.ago])
-end
