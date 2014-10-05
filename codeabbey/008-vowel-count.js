cc5d77cd29903ed3ee7148c0dc31dadd8fb17f1c
Louis <louis@lpil.co.uk>
2014-10-05 18:22:30 +0100

    codeabbey 7 8

---
+++ codeabbey/008-vowel-count.js
@@ -0,0 +1,20 @@
+#!/usr/bin/env node
+
+
+var input, output;
+
+input = require('fs')
+        .readFileSync('./input.txt', 'utf8')
+        .split('\n')
+        .slice(1, -1);
+
+output = input
+          .map(function(string) {
+            return string.split('')
+              .filter(function(cha) {
+                return /[aeiouy]/.test(cha);
+              }).length;
+          })
+          .join(' ');
+
+console.log(output);
