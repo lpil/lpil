378ad92e62392592064364b524cb32ad6dc89e55
Louis Pilfold <louis@lpil.uk>
2014-10-26 20:58:02 +0000

    cis194 1

---
+++ cis194/01-hw.hs
@@ -0,0 +1,15 @@
+{-
+Name: <your name here>
+Collaborators: <your collaborators here, or "none">
+Notes: <any particular notes about your work -- what you struggled with,
+        what's not working, what's really cool, etc.>
+-}
+
+module HW01 where         -- We'll learn more about this later
+
+isThisWorking :: String
+isThisWorking = "Yes"
+-- Load this file into GHCi (say, with `ghci HW01.hs`) and type
+-- `isThisWorking` at the prompt. GHCi will tell you whether it's working!
+
+-- Put your work below.
