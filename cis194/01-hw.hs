92134d7c86446ee52d1f9b2e8487ac70c0365c17
Louis Pilfold <louis@lpil.uk>
2014-10-26 21:15:31 +0000

    cis194 1 hw: lastDigit, dropLastDigit

--- cis194/01-hw.hs
+++ cis194/01-hw.hs
@@ -1,15 +1,45 @@
-{-
-Name: <your name here>
-Collaborators: <your collaborators here, or "none">
-Notes: <any particular notes about your work -- what you struggled with,
-        what's not working, what's really cool, etc.>
--}
+-- http://www.seas.upenn.edu/~cis194/hw/01-intro.pdf
 
 module HW01 where         -- We'll learn more about this later
 
-isThisWorking :: String
-isThisWorking = "Yes"
--- Load this file into GHCi (say, with `ghci HW01.hs`) and type
--- `isThisWorking` at the prompt. GHCi will tell you whether it's working!
+--
+-- Validate credit card numbers
+--
 
--- Put your work below.
+-- In this section, you will implement the validation algorithm for credit
+-- cards. It follows these steps:
+
+-- Double the value of every second digit beginning from the right. That
+-- is, the last digit is unchanged; the second-to-last digit is dou- bled;
+-- the third-to-last digit is unchanged; and so on. For example, [1,3,8,6]
+-- becomes [2,3,16,6]
+
+-- Add the digits of the doubled values and the undoubled dig- its from the
+-- original number. For example, [2,3,16,6] becomes 2+3+1+6+6 = 18
+
+-- Calculate the remainder when the sum is divided by 10 For the above
+-- example, the remainder would be 8
+
+-- If the result equals 0, then the number is valid.
+
+--
+-- Exercise 1
+--
+
+-- We first need to be able to break up a number into its last digit and
+-- the rest of the number. Write these functions:
+
+-- lastDigit :: Integer -> Integer
+-- dropLastDigit :: Integer -> Integer
+
+-- lastDigit 123 == 3
+-- lastDigit 0 == 0
+
+-- dropLastDigit 123 == 12
+-- dropLastDigit 5 == 0
+
+lastDigit :: Integer -> Integer
+lastDigit n = mod n 10
+
+dropLastDigit :: Integer -> Integer
+dropLastDigit n = div n 10
