4b9e0edbdbeca6d6305d1be18892412f75ddbe51
Louis Pilfold <louis@lpil.uk>
2014-10-26 16:41:15 +0000

    map foldl

--- map.hs
+++ map.hs
@@ -1,3 +1,9 @@
+-- Explicit recursion
 map2 :: (a -> b) -> [a] -> [b]
 map2 _ [] = []
 map2 f (x:xs) = f x : map f xs
+
+-- Foldl
+map3 :: (a -> b) -> [a] -> [b]
+map3 f xs = reverse $ foldl comp [] xs
+  where comp acc x = f x : acc
