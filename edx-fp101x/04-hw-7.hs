34f027922902d7722216a5243f63c090d96a6a20
Louis Pilfold <louis@lpil.uk>
2014-10-31 13:38:21 +0000

    fp101x 3 hw 7 finish
    Dear god, I did some stupid things there...

--- edx-fp101x/04-hw-7.hs
+++ edx-fp101x/04-hw-7.hs
@@ -10,8 +10,9 @@ int2let n = chr (ord 'a' + n)
 
 shift :: Int -> Char -> Char
 shift n c
-  | isLower c = int2let $ (let2int c + n) `mod` 26
+  | isLower c = int2let $           (let2int     c       + n) `mod` 26
+  | isUpper c = toUpper $ int2let $ (let2int (toLower c) + n) `mod` 26
   | otherwise = c
 
 encode :: Int -> String -> String
-encode n xs = [shift n x | x <- xs ]
+encode n xs = [shift n x | x <- xs]
