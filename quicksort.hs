83cfde5786307da4ebf4766a2cbed5af76548fe5
Louis Pilfold <louis@lpil.uk>
2014-11-03 20:24:31 +0000

    tweak quicksort indentation

--- quicksort.hs
+++ quicksort.hs
@@ -1,5 +1,5 @@
 quicksort :: (Ord a) => [a] -> [a]
-quicksort [] = []
+quicksort []     = []
 quicksort (x:xs) = smaller ++ [x] ++ bigger
   where smaller = quicksort $ filter (<= x) xs
         bigger  = quicksort $ filter (>  x) xs
