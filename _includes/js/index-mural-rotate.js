e34228d5bfd072e740c07af8639a3d031bef98ee
Louis Pilfold <louis@lpil.uk>
2014-11-12 10:54:11 +0000

    Move JS into submodule

--- _includes/js/index-mural-rotate.js
+++
@@ -1,8 +0,0 @@
-(function() {
-  var texts = document.getElementsByClassName('js-index__mural__text');
-
-  window.setInterval(function() {
-    texts[0].parentNode.insertBefore(
-        texts[0], texts[texts.length - 1].nextSibling);
-  }, 8000);
-}());
