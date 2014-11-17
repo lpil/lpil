bc07eb513121ba4fbfd22e9cd24985514d9b856d
Louis Pilfold <louis@lpil.uk>
2014-11-17 13:12:35 +0000

    hw 7 start

--- edx-fp101x/07-hw.hs
+++ edx-fp101x/07-hw.hs
@@ -1,9 +1,7 @@
 -- Functional parsing library from chapter 8 of Programming in Haskell,
 -- Graham Hutton, Cambridge University Press, 2007.
 
-
 module Parsing where
-
 
 import Data.Char
 import Control.Monad
@@ -12,41 +10,43 @@ infixr 5 +++
 
 -- The monad of parsers
 
-newtype Parser a =  P (String -[(a,String)])
+newtype Parser a = P (String -> [(a,String)])
 
 instance Monad Parser where
-   return v =  P (\inp -[(v,inp)])
-   p >>= f  =  undefined -- you will need to implement this in an exercise
+   return v = P (\inp -> [(v,inp)])
+   p >>= f  = undefined -- you will need to implement this in an exercise
 
 instance MonadPlus Parser where
-   mzero       =  P (\inp -[])
-   p `mplus` q =  P (\inp -case parse p inp of
-                                []        -parse q inp
-                                [(v,out)] -[(v,out)])
+   mzero       = P (\_   -> [])
+   p `mplus` q = P (\inp -> case parse p inp of
+                                []        -> parse q inp
+                                [(v,out)] -> [(v,out)])
 
 -- Basic parsers
 
 failure :: Parser a
-failure =  mzero
+failure = mzero
 
-item  :: Parser Char
-item  =  P (\inp -case inp of
-                       []     -[]
-                       (x:xs) -[(x,xs)])
+item :: Parser Char
+item = P (\inp -> case inp of
+                      []     -> []
+                      (x:xs) -> [(x,xs)])
 
-parse :: Parser a -String -[(a,String)]
-parse (P p) inp =  p inp
+parse :: Parser a -> String -> [(a,String)]
+parse (P p) inp = p inp
 
 -- Choice
 
-(+++) :: Parser a -Parser a -Parser a
-p +++ q =  p `mplus` q
+(+++) :: Parser a -> Parser a -> Parser a
+p +++ q = p `mplus` q
 
 -- Derived primitives
 
-sat :: (Char -Bool) -Parser Char
+sat :: (Char -> Bool) -> Parser Char
 sat p = do x <- item
-          if p x then return x else failure
+           if p x
+             then return x
+             else failure
 
 digit :: Parser Char
 digit = sat isDigit
@@ -63,55 +63,55 @@ letter = sat isAlpha
 alphanum :: Parser Char
 alphanum = sat isAlphaNum
 
-char :: Char -Parser Char
-char x =  sat (== x)
+char :: Char -> Parser Char
+char x = sat (== x)
 
-string :: String -Parser String
-string []     =  return []
-string (x:xs) =  do char x
-                    string xs
-                    return (x:xs)
+string :: String -> Parser String
+string []     = return []
+string (x:xs) = do char x
+                   string xs
+                   return (x:xs)
 
-many :: Parser a -Parser [a]
+many :: Parser a -> Parser [a]
 many p = many1 p +++ return []
 
-many1 :: Parser a -Parser [a]
-many1 p =  do v  <- p
-              vs <- many p
-              return (v:vs)
+many1 :: Parser a -> Parser [a]
+many1 p = do v  <- p
+             vs <- many p
+             return (v:vs)
 
 ident :: Parser String
-ident =  do x  <- lower
-            xs <- many alphanum
-            return (x:xs)
+ident = do x  <- lower
+           xs <- many alphanum
+           return (x:xs)
 
 nat :: Parser Int
 nat = do xs <- many1 digit
-        return (read xs)
+         return (read xs)
 
 int :: Parser Int
-int =  undefined -- you will need to implement this in an exercise
+int = undefined -- you will need to implement this in an exercise
 
 space :: Parser ()
-space =  do many (sat isSpace)
-            return ()
+space = do many (sat isSpace)
+           return ()
 
 -- Ignoring spacing
 
-token :: Parser a -Parser a
-token p =  do space
-              v <- p
-              space
-              return v
+token :: Parser a -> Parser a
+token p = do space
+             v <- p
+             space
+             return v
 
 identifier :: Parser String
-identifier =  token ident
+identifier = token ident
 
 natural :: Parser Int
-natural =  token nat
+natural = token nat
 
 integer :: Parser Int
-integer =  token int
+integer = token int
 
-symbol :: String -Parser String
+symbol :: String -> Parser String
 symbol xs = token (string xs)
