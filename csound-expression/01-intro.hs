a04e7ca425fbf839a8518250fb8c17a1ce5f3f19
Louis Pilfold <louis@lpil.uk>
2014-12-05 10:24:24 +0000

    csound-expression intro

---
+++ csound-expression/01-intro.hs
@@ -0,0 +1,11 @@
+-- github.com/anton-k/csound-expression/blob/master/tutorial/chapters/Intro.md
+
+module Intro where
+
+import Csound.Base
+
+-- Lets play a sine wave at 440
+zero :: IO ()
+zero = dac $ osc 440
+
+-- Magic!
