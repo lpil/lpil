d9719aa5b4ae349d6a8a2600dc965dffb761629e
Louis Pilfold <louis@lpil.uk>
2014-12-09 23:51:01 +0000

    Move logic from draw to state

--- quil-experiments/src/quil_experiments/two/dynamic.clj
+++ quil-experiments/src/quil_experiments/two/dynamic.clj
@@ -5,8 +5,8 @@
 (defn cycles [num] (* num 2 (. Math PI)))
 
 (defn increment [previous-state steps]
-   (mod (+ (/ 1 steps) previous-state)
-        1))
+  (mod (+ (/ 1 steps) previous-state)
+       1))
 
 (defn setup []
   (frame-rate 30)
@@ -16,16 +16,19 @@
    :two 1})
 
 (defn update [state]
-  {:one (increment (:one state) 1000)
-   :two (increment (:two state)  100)})
+  (let [one (increment (:one state) 1000)
+        two (increment (:two state)  100)
+
+        size (+ 300
+              (* 90 (Math/sin (cycles one)))
+              (* 10 (Math/sin (cycles two))))]
+
+    {:one one
+     :two two
+     :size size}))
 
 (defn draw [state]
   (background 240)
-
   (text (str (round (current-frame-rate))) 5 15)
   (fill 0)
-
-  (let [size (+ 300
-                (* 90 (Math/sin (cycles (:one state))))  
-                (* 10 (Math/sin (cycles (:two state)))))]
-    (ellipse (/ (width) 2) (/ (height) 2) size size)))
+  (ellipse (/ (width) 2) (/ (height) 2) (:size state) (:size state)))
