9c57c2b7ad3a76773725ba336bcfc030f9d07f75
Louis Pilfold <louis@lpil.uk>
2014-12-10 14:33:37 +0000

    remove some comments

--- quil-experiments/src/quil_experiments/two/core.clj
+++ quil-experiments/src/quil_experiments/two/core.clj
@@ -6,11 +6,7 @@
 (q/defsketch quil-experiments
   :title "Hello, Quil!"
   :size [500 500]
-  ; setup function called only once, during sketch initialization.
   :setup dynamic/setup
-  ; update is called on each iteration before draw is called.
-  ; It updates sketch state.
   :update dynamic/update
   :draw dynamic/draw
-  ; This sketch uses functional-mode middleware.
   :middleware [m/fun-mode])
