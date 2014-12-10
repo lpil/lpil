868e5b15babb753c5c3c7047b7c5f2eb9f12cdb4
Louis Pilfold <louis@lpil.uk>
2014-12-10 11:15:06 +0000

    Thank you kibit

--- quil-experiments/src/quil_experiments/one/dynamic.clj
+++ quil-experiments/src/quil_experiments/one/dynamic.clj
@@ -14,10 +14,10 @@
 
 (defn draw [state]
   (fill 255 10)
-  (rect -1 -1 (+ 1 (width)) (+ 1 (height)))
+  (rect -1 -1 (inc (width)) (inc (height)))
 
-  (if (mouse-pressed?)
-    (do (line 0       0        (mouse-x) (mouse-y))
-        (line 0       (height) (mouse-x) (mouse-y))
-        (line (width) 0        (mouse-x) (mouse-y))
-        (line (width) (height) (mouse-x) (mouse-y)))))
+  (when (mouse-pressed?)
+    (line 0       0        (mouse-x) (mouse-y))
+    (line 0       (height) (mouse-x) (mouse-y))
+    (line (width) 0        (mouse-x) (mouse-y))
+    (line (width) (height) (mouse-x) (mouse-y))))
