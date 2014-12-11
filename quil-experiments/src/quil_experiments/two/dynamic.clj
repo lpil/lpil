33ae7257f7c63584dc87414d930b52bfe9d5d6ef
Louis Pilfold <louis@lpil.uk>
2014-12-11 13:18:08 +0000

    Pulse alpha channel with fast osc

--- quil-experiments/src/quil_experiments/two/dynamic.clj
+++ quil-experiments/src/quil_experiments/two/dynamic.clj
@@ -2,11 +2,14 @@
   (:use [quil.core])
   (:require [quil.middleware :as m]))
 
-(defn cycles [num] (* num 2 (Math/PI)))
+(defn ratio->radians [num] (* num 2 (Math/PI)))
+
+(defn ratio->sine [ratio] (Math/sin (ratio->radians ratio)))
 
 (defn increment [previous-state steps]
   (mod (+ (/ 1 steps) previous-state)
        1))
+
 
 (defn setup []
   (frame-rate 30)
@@ -14,17 +17,21 @@
    :two 1})
 
 (defn update [state]
-  (let [one (increment (:one state) 1500)
-        two (increment (:two state)  150)
+  (let [slow-osc  (increment (:one state) 1500)
+        fast-osc  (increment (:two state)  150)
+        f-osc-255 (round (+ 100 (* 100 (ratio->sine two))))
         size (+ 280
-                (* 125 (Math/sin (cycles one)))
-                (* 25 (Math/sin (cycles two))))]
-    {:one one
-     :two two
-     :size size}))
+                (* 125 (ratio->sine one))
+                (* 25 (ratio->sine two)))]
+    {:colour fast-osc-255
+     :size   size}))
 
 (defn draw [state]
   (background 240)
-  (text (str (round (:size state))) 5 15)
   (fill 0)
+  (text (str (round (:size state))) 5 15)
+  (text (str (:colour state)) 5 35)
+
+  (no-stroke)
+  (fill 0 (:colour state))
   (ellipse (/ (width) 2) (/ (height) 2) (:size state) (:size state)))
