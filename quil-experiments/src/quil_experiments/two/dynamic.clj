c254292cdd038069f97dc4e17ee3907f822b2910
Louis Pilfold <louis@lpil.uk>
2014-12-11 13:25:34 +0000

    Bugfix: Lots of broken variable names

--- quil-experiments/src/quil_experiments/two/dynamic.clj
+++ quil-experiments/src/quil_experiments/two/dynamic.clj
@@ -13,18 +13,20 @@
 
 (defn setup []
   (frame-rate 30)
-  {:one 1
-   :two 1})
+  {:slow-osc 0
+   :fast-osc 0})
 
 (defn update [state]
-  (let [slow-osc  (increment (:one state) 1500)
-        fast-osc  (increment (:two state)  150)
-        f-osc-255 (round (+ 100 (* 100 (ratio->sine two))))
+  (let [slow-osc  (increment (:slow-osc state) 1500)
+        fast-osc  (increment (:fast-osc state)  150)
+        f-osc-255 (round (+ 80 (* 80 (ratio->sine fast-osc))))
         size (+ 280
-                (* 125 (ratio->sine one))
-                (* 25 (ratio->sine two)))]
-    {:colour fast-osc-255
-     :size   size}))
+                (* 125 (ratio->sine slow-osc))
+                (*  25 (ratio->sine fast-osc)))]
+    {:slow-osc slow-osc
+     :fast-osc fast-osc
+     :colour   f-osc-255
+     :size     size}))
 
 (defn draw [state]
   (background 240)
@@ -33,5 +35,5 @@
   (text (str (:colour state)) 5 35)
 
   (no-stroke)
-  (fill 0 (:colour state))
+  (fill 0 (- 255 (:colour state)))
   (ellipse (/ (width) 2) (/ (height) 2) (:size state) (:size state)))
