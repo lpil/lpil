56ee945e59492c4cd267ad34ce8624627821e595
Louis Pilfold <louis@lpil.uk>
2014-12-15 09:28:59 +0000

    lein-test-refresh

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,3 +1,4 @@
-{:user {:plugins [[jonase/eastwood "0.2.0"]
-                  [lein-kibit "0.0.8"]
-                  [cider/cider-nrepl "0.8.1"]]}}
+{:user {:plugins [[cider/cider-nrepl "0.8.1"]
+                  [com.jakemccrary/lein-test-refresh  "0.5.4"]
+                  [jonase/eastwood "0.2.0"]
+                  [lein-kibit "0.0.8"]]}}
