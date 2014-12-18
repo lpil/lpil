d76c1116180ee6f36905fee96e5b3d1649417285
Louis Pilfold <louis@lpil.uk>
2014-12-18 10:49:47 +0000

    Can get query for followers

--- upcase-intermediate-rails/shouter/app/models/user.rb
+++ upcase-intermediate-rails/shouter/app/models/user.rb
@@ -3,6 +3,13 @@ class User < ActiveRecord::Base
 
   has_many :shouts
 
-  has_many :following_relationships, foreign_key: :follower_id
-  has_many :followed_users, through: :following_relationships
+  has_many :followed_user_relationships,
+           foreign_key: :follower_id,
+           class_name: 'FollowingRelationship'
+  has_many :followed_users, through: :followed_user_relationships
+
+  has_many :follower_relationships,
+           foreign_key: :followed_user_id,
+           class_name: 'FollowingRelationship'
+  has_many :followers, through: :follower_relationships
 end
