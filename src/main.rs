faa35e922baf1a2e756cbe2d22a10d8d406ad47d
Louis Pilfold <louis@lpil.uk>
2014-12-29 01:20:42 +0000

    Start is_winning_board fn. same_player fn

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,19 @@
-fn main() {
-    type Board = [int; 9];
+type Board = [int; 9];
 
+fn main() {
     let board: Board = [
         0,0,0,
         0,0,0,
         0,0,0
     ];
 
-    println!("{}", board);
+    println!("{}", is_winning_board(board));
+}
+
+fn is_winning_board(board: Board) -> bool {
+    fn same_player([x, y, z]: [int; 3]) -> bool {
+        x == y && y == z
+    }
+
+    same_player([1i,2i,3i])
 }
