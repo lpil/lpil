978a4940b7b400b29ebd03a5f086ddead0a6b696
Louis Pilfold <louis@lpil.uk>
2014-12-31 13:56:47 +0000

    Refactor into modules, Game struct

--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,23 @@
-pub type Board = [Cell; 9];
-pub type Player = char;
-pub type Cell = Option<Player>;
+#![feature(globs)]
+use ttt_game::*;
 
+mod ttt_game;
+
+#[cfg(not(test))]
 fn main() {
-    let board: Board = [
-        None,None,None,
-        None,None,None,
-        None,None,None
-    ];
+    let game = Game {
+        board: [
+            None,None,None,
+            None,None,None,
+            None,None,None
+        ]
+    };
 
-    println!("{}", board);
+    println!("{}", game.board);
 
-    match won(board) {
+    match game.won() {
         Some(x) => println!("{} wins", x),
         _ => println!("No one won")
     }
 }
 
-pub fn won(board: Board) -> Option<Player> {
-    let winning_patterns: [[uint; 3]; 8] = [
-        // horizontal
-        [0,1,2],
-        [3,4,5],
-        [6,7,8],
-        // Vertical
-        [0,3,6],
-        [1,4,7],
-        [2,5,8],
-        // Diagonal
-        [0,4,8],
-        [6,4,2]
-    ];
-
-    for pattern in winning_patterns.iter() {
-        let x = pattern[0];
-        let y = pattern[1];
-        let z = pattern[2];
-
-        if board[x] == board[y] && board[y] == board[z] {
-            return board[x]
-        }
-    }
-    None
-}
---
+++ src/ttt_game.rs
@@ -0,0 +1,36 @@
+pub type Player = char;
+pub type Cell = Option<Player>;
+
+pub struct Game {
+    pub board: [Cell; 9]
+}
+
+impl Game {
+    pub fn won(&self) -> Option<Player> {
+        let board = self.board;
+        let winning_patterns: [[uint; 3]; 8] = [
+            // horizontal
+            [0,1,2],
+            [3,4,5],
+            [6,7,8],
+            // Vertical
+            [0,3,6],
+            [1,4,7],
+            [2,5,8],
+            // Diagonal
+            [0,4,8],
+            [6,4,2]
+        ];
+
+        for pattern in winning_patterns.iter() {
+            let x = pattern[0];
+            let y = pattern[1];
+            let z = pattern[2];
+
+            if board[x] == board[y] && board[y] == board[z] {
+                return board[x]
+            }
+        }
+        None
+    }
+}
