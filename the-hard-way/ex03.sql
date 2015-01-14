3710524b87a06b02994570714da7c134e5706ec1
Louis Pilfold <louis@lpil.uk>
2015-01-14 17:10:44 +0000

    A bunch of things

---
+++ the-hard-way/ex03.sql
@@ -0,0 +1,10 @@
+/* Run on db 2 */
+INSERT INTO person (id, first_name, last_name, age)
+  VALUES (0, "Zed", "Shaw", 37);
+
+INSERT INTO pet (id, name, species, age, dead)
+  VALUES (0, "Fluffy", "Unicorn", 1000, 0);
+
+INSERT INTO pet VALUES (1, "Gigantor", "Robot", 1, 1);
+INSERT INTO pet VALUES (2, "Dinbar", "Hopalong", 11, 0);
+INSERT INTO pet VALUES (3, "Foop", "Parrot", 23, 0);
