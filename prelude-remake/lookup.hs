ba11bffff7ea831393b828e5b8e8d5934528a835
Louis Pilfold <louis@lpil.uk>
2015-01-25 23:00:39 +0000

    prelude notElem, clear arg names

--- prelude-remake/lookup.hs
+++ prelude-remake/lookup.hs
@@ -3,7 +3,7 @@ module Lookup where
 import Prelude hiding (lookup)
 
 lookup :: Eq a => a -> [(a, b)] -> Maybe b
-lookup _ []           = Nothing
-lookup x ((y, z):rest)
-  | x == y    = Just z
+lookup _ [] = Nothing
+lookup x ((key, val):rest)
+  | x == key  = Just val
   | otherwise = lookup x rest
