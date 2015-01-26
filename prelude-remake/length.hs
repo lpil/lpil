a27901fce625da8473599f17606f719e5abd6f21
Louis Pilfold <louis@lpil.uk>
2015-01-26 18:17:52 +0000

    prelude: New length

--- prelude-remake/length.hs
+++ prelude-remake/length.hs
@@ -3,4 +3,11 @@ module Length where
 import Prelude hiding (length)
 
 length :: Integral b => [a] -> b
-length = foldr (\_ count -> succ count) 0
+length xs =
+    let loop []     count = count
+        loop (_:ys) count = loop ys $ succ count
+    in loop xs 0
+
+-- I think this builds up a lot of thunks and uses a lot of memory.
+length2 :: Integral b => [a] -> b
+length2 = foldr (\_ count -> succ count) 0
