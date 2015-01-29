be8c6e8ecfeec0934eddf46fd81cfd0567dc14db
Louis Pilfold <louis@lpil.uk>
2015-01-29 17:18:30 +0000

    eggheads angular filters

---
+++ egghead-angular/02-filters/main.js
@@ -0,0 +1,31 @@
+var app = angular.module('myApp', [])
+
+app.controller('MainCtrl', function($scope) {
+  $scope.categories = [
+    {"id": 0, "name": "Development"},
+    {"id": 1, "name": "Design"},
+    {"id": 2, "name": "Exercise"},
+    {"id": 3, "name": "Humor"}
+  ];
+
+  $scope.bookmarks = [
+    {"id": 0, "title": "AngularJS", "url": "http://angularjs.org", "category": "Development" },
+    {"id": 1, "title": "Egghead.io", "url": "http://angularjs.org", "category": "Development" },
+    {"id": 2, "title": "A List Apart", "url": "http://alistapart.com/", "category": "Design" },
+    {"id": 3, "title": "One Page Love", "url": "http://onepagelove.com/", "category": "Design" },
+    {"id": 4, "title": "MobilityWOD", "url": "http://www.mobilitywod.com/", "category": "Exercise" },
+    {"id": 5, "title": "Robb Wolf", "url": "http://robbwolf.com/", "category": "Exercise" },
+    {"id": 6, "title": "Senor Gif", "url": "http://memebase.cheezburger.com/senorgif", "category": "Humor" },
+    {"id": 7, "title": "Wimp", "url": "http://wimp.com", "category": "Humor" },
+    {"id": 8, "title": "Dump", "url": "http://dump.com", "category": "Humor" }
+  ];
+
+  $scope.setCurrentCategory = function setCurrentCategory(category) {
+    $scope.currentCategory = category;
+  };
+
+  $scope.isCurrentCategory = function isCurrentCategory(category) {
+    return $scope.currentCategory !== null
+      && category.name === $scope.currentCategory.name;
+  };
+});
