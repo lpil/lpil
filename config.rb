d7be6883042e376b4a7fbd7ebdb5c2042886faee
Louis Pilfold <louis@lpil.uk>
2015-01-31 23:36:15 +0000

    reformat normalizer a little

--- config.rb
+++ config.rb
@@ -10,7 +10,8 @@ set :js_dir, 'js'
 set :images_dir, 'images'
 
 set :markdown_engine, :redcarpet
-set :markdown, fenced_code_blocks: true, smartypants: true
+set :markdown, fenced_code_blocks: true,
+               smartypants: true
 
 configure :build do
   activate :minify_css
