31b8af8291e04a617d2e35b86c39e20235f01b3e
Louis Pilfold <louis@lpil.uk>
2015-03-05 12:55:49 +0000

    exercism rna transcription

---
+++ exercism-elixir/rna-transcription/dna.exs
@@ -0,0 +1,23 @@
+defmodule DNA do
+  @doc """
+  Transcribes a character list representing DNA nucleotides to RNA
+
+  ## Examples
+
+  iex> DNA.to_rna('ACTG')
+  'UGAC'
+  """
+  @spec to_rna([char]) :: [char]
+  def to_rna(dna) do
+    Enum.map dna, &nucleotide_rna_to_dna(&1)
+  end
+
+  defp nucleotide_rna_to_dna(nucleotide) do
+    case nucleotide do
+      ?G -> ?C
+      ?C -> ?G
+      ?T -> ?A
+      ?A -> ?U
+    end
+  end
+end
