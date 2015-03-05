12d880ef05a0bd18927f8e5fb9e54ad25e8e1364
Louis Pilfold <louis@lpil.uk>
2015-03-05 22:25:52 +0000

    exercism leap

---
+++ exercism-elixir/leap/leap_test.exs
@@ -0,0 +1,27 @@
+if System.get_env("EXERCISM_TEST_EXAMPLES") do
+  Code.load_file("example.exs")
+else
+  Code.load_file("leap.exs")
+end
+
+ExUnit.start
+
+defmodule LeapTest do
+  use ExUnit.Case, async: true
+
+  test "vanilla leap year" do
+    assert Year.leap_year?(1996)
+  end
+
+  test "any old year" do
+    assert ! Year.leap_year?(1997)
+  end
+
+  test "century" do
+    assert ! Year.leap_year?(1900)
+  end
+
+  test "exceptional century" do
+    assert Year.leap_year?(2400)
+  end
+end
