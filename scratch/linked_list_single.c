c8d344a76dba766b6f2c47a3fb09f0d5783c9874
Louis Pilfold <louis@lpil.uk>
2015-09-14 23:25:30 +0100

    Print lists

--- scratch/linked_list_single.c
+++ scratch/linked_list_single.c
@@ -13,10 +13,35 @@ struct cons_cell *List_new(int n)
   return cell;
 }
 
+struct cons_cell *List_prepend(struct cons_cell *list, int n)
+{
+  struct cons_cell *head = malloc( sizeof(struct cons_cell) );
+  *head = (struct cons_cell) { .value = n, .next = list };
+  return head;
+}
+
+
+void List_print(struct cons_cell *head)
+{
+  printf("(");
+  struct cons_cell *next;
+  next = head->next;
+  while(next != NULL) {
+    printf("%d, ", head->value);
+    head = next;
+    next = head->next;
+  }
+  printf("%d)\n", head->value);
+}
 
 int main(void)
 {
-  struct cons_cell *cell = List_new(2);
-  printf("Value is %d!\n", cell->value);
+  struct cons_cell *list = List_new(1);
+  list = List_prepend(list, 2);
+  list = List_prepend(list, 3);
+  list = List_prepend(list, 4);
+  list = List_prepend(list, 5);
+  list = List_prepend(list, 6);
+  List_print(list);
   return 0;
 }
