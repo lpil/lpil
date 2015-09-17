4cc73538a3e3cee6b7107300987cee231b8766a1
Louis Pilfold <louis@lpil.uk>
2015-09-17 21:58:58 +0100

    Growable vector

---
+++ scratch/vector.c
@@ -0,0 +1,64 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+struct vector {
+  char *contents;
+  int size;
+  int capacity;
+};
+
+struct vector *Vector_new(int capacity)
+{
+  char *contents     = malloc( capacity * sizeof(char) );
+  struct vector *vec = malloc( sizeof(struct vector) );
+  *vec = (struct vector) {
+    .contents = contents,
+    .capacity = capacity,
+    .size = 0,
+  };
+  return vec;
+}
+
+
+struct vector *Vector_expand(struct vector *vec)
+{
+  vec->capacity *= 2;
+  char *contents = malloc( vec->capacity * sizeof(char) );
+  char *old = vec->contents;
+  int i = vec->size;
+  while (i--) {
+    contents[i] = old[i];
+  }
+  vec->contents = contents;
+  return vec;
+}
+
+
+struct vector *Vector_push(struct vector *vec, char data)
+{
+  if (vec->size >= vec->capacity) {
+    vec = Vector_expand(vec);
+  }
+  vec->contents[vec->size] = data;
+  vec->size++;
+  return vec;
+}
+
+
+
+int main(void)
+{
+  int i;
+  struct vector *v = Vector_new(1);
+  v = Vector_push(v, 'H');
+  v = Vector_push(v, 'e');
+  v = Vector_push(v, 'l');
+  v = Vector_push(v, 'l');
+  v = Vector_push(v, 'o');
+  v = Vector_push(v, '!');
+  v = Vector_push(v, '\n');
+  for (i = 0; i < v->size; ++i) {
+    printf("%c", v->contents[i]);
+  }
+  return 0;
+}
