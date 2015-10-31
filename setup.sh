78d0feae69c93177df7524fdd3c20ba35c1dd0ef
Louis Pilfold <louis@lpil.uk>
2015-10-31 18:50:01 +0000

    Make setup.sh safer

--- setup.sh
+++ setup.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+set -e # Abort on error
+set -u # Error on unset varibles
+set -o pipefail
+
 # install Vundle package manager
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
