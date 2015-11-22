37c95fcd46bb4b21c19597b50c8805a4ac78e278
Louis Pilfold <louis@lpil.uk>
2015-11-22 15:20:23 +0000

    Symlink neovim config

--- setup.sh
+++ setup.sh
@@ -7,6 +7,11 @@ set -o pipefail
 # install Plug package manager
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
+# Symlink neovim config into place
+mkdir -p ${XDG_CONFIG_HOME:=$HOME/.config}
+ln -s ~/.vim $XDG_CONFIG_HOME/nvim
+ln -s ~/.vimrc $XDG_CONFIG_HOME/nvim/init.vim
+
 # Setup undo history
 mkdir -p ~/.vim/undo/
 
