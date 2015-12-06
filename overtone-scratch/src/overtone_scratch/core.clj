d10cdae402d11f408a97f0efa42843bae7c3be1d
Louis Pilfold <louis@lpil.uk>
2015-12-06 23:38:09 +0000

    Making noise

--- overtone-scratch/src/overtone_scratch/core.clj
+++ overtone-scratch/src/overtone_scratch/core.clj
@@ -5,14 +5,38 @@
 
 (demo (sin-osc))
 
-(demo 7 
-      (pan2 (lpf (mix (saw [50 (line 100 1600 5) 101 100.5]))
-                 (lin-lin (lf-tri (line 2 20 5)) -1 1 400 4000)) 
-            0))
-
 (defn piano-later [n]
   (at (+ (now) n) (piano 38)))
+
+(defn play-chord [a-chord]
+  (doseq [note a-chord] (piano note)))
 
 (piano-later 1000)
 
 (map piano-later [0 1600 3200])
+
+(play-chord (chord :c4 :minor))
+(play-chord (chord :d4 :minor))
+
+(let [time (now)]
+  (at (+ time 0000) (play-chord (chord :f4 :minor)))
+  (at (+ time 1000) (play-chord (chord :e4 :minor)))
+  (at (+ time 3000) (play-chord (chord :d4 :minor)))
+  (at (+ time 4000) (play-chord (chord :c4 :minor))))
+
+(defn play [time notes time-sep]
+  (let [note (first notes)
+        next-time (+ time time-sep)]
+    (piano (- note 12))
+    (apply-at next-time play [next-time (rest notes) time-sep])))
+
+(defn play [time notes time-sep])
+
+(def my-scale 
+  (reverse (scale :Bb4 :minor)))
+
+(let [time (now)]
+  (play (now) (cycle my-scale) 280)
+  (play (now) (cycle my-scale) 285))
+
+(stop)
