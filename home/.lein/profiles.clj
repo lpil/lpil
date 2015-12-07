b04494f2b84a4cb573fe3679748fe2b5d3d7bccf
Louis Pilfold <louis@lpil.uk>
2015-12-07 23:45:02 +0000

    Clojure: +midje, bump cider, -others

--- home/.lein/profiles.clj
+++ home/.lein/profiles.clj
@@ -1,4 +1,2 @@
-{:user {:plugins [[cider/cider-nrepl "0.8.1"]
-                  [com.jakemccrary/lein-test-refresh  "0.5.4"]
-                  [jonase/eastwood "0.2.0"]
-                  [lein-kibit "0.0.8"]]}}
+{:user {:plugins [[cider/cider-nrepl "0.10.0"]
+                  [lein-midje "3.1.3"]]}}
