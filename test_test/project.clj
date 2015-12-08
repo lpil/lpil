3749061b3488d8a860a2cc83952c8f17e9800783
Louis Pilfold <louis@lpil.uk>
2015-12-08 00:04:18 +0000

    midje is really nice.

---
+++ test_test/project.clj
@@ -0,0 +1,5 @@
+(defproject test_test "0.0.1-SNAPSHOT"
+  :description "Cool new project to do things and stuff"
+  :dependencies [[org.clojure/clojure "1.7.0"]]
+  :profiles {:dev {:dependencies [[midje "1.5.1"]]}})
+  
