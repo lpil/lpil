eecb45336181b97decdd8bb1b1dee9a7e5836346
Louis Pilfold <louis@lpil.uk>
2016-01-20 23:20:44 +0000

    Lein plugins!

--- home/.lein/profiles.clj
+++ home/.lein/profiles.clj
@@ -1,2 +1,3 @@
 {:user {:plugins [[cider/cider-nrepl "0.10.0"]
-                  [lein-midje "3.1.3"]]}}
+                  [com.jakemccrary/lein-test-refresh "0.12.0"]
+                  [venantius/ultra "0.3.4"]]}}
