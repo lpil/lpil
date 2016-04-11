7db46805df851ae331f7fee71ad2b58707738bc0
Louis Pilfold <louis@lpil.uk>
2016-04-11 17:05:52 +0100

    Add test & dev deps

--- mix.exs
+++ mix.exs
@@ -12,26 +12,31 @@ defmodule BlockParty.Mixfile do
      deps: deps]
   end
 
-  # Configuration for the OTP application.
-  #
-  # Type `mix help compile.app` for more information.
   def application do
     [mod: {BlockParty, []},
      applications: [:phoenix, :phoenix_html, :cowboy, :logger, :gettext]]
   end
 
-  # Specifies which paths to compile per environment.
   defp elixirc_paths(:test), do: ["lib", "web", "test/support"]
   defp elixirc_paths(_),     do: ["lib", "web"]
 
-  # Specifies your project dependencies.
-  #
-  # Type `mix help deps` for examples and options.
   defp deps do
-    [{:phoenix, "~> 1.1.4"},
-     {:phoenix_html, "~> 2.4"},
-     {:phoenix_live_reload, "~> 1.0", only: :dev},
-     {:gettext, "~> 0.9"},
-     {:cowboy, "~> 1.0"}]
+    [
+      # Web framework
+      {:phoenix, "~> 1.1.4"},
+      # HTML view helpers
+      {:phoenix_html, "~> 2.4"},
+      # Page reloading in dev
+      {:phoenix_live_reload, "~> 1.0", only: :dev},
+      # Translation macros
+      {:gettext, "~> 0.9"},
+      # Web server
+      {:cowboy, "~> 1.0"},
+
+      # Code style linter
+      {:dogma, "~> 0.0", only: [:dev, :test]},
+      # Automatic test runner
+      {:mix_test_watch, "~> 0.0", only: :dev},
+    ]
   end
 end
