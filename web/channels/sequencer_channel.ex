d122f4c0ba6df95f4708d4ad128d38a9ac493869
Louis Pilfold <louis@lpil.uk>
2016-04-13 17:31:23 +0100

    Send grid state on join

--- web/channels/sequencer_channel.ex
+++ web/channels/sequencer_channel.ex
@@ -6,20 +6,35 @@ defmodule BlockParty.SequencerChannel do
   alias BlockParty.SequencerState, as: SeqState
 
   def join("sequencers:lobby", _message, socket) do
+    send self, :after_join
     {:ok, socket}
   end
   def join("sequencers:" <> _private_room_id, _params, _socket) do
     {:error, %{ reason: "unknown room" }}
   end
 
+  # Send current grid state after a user joins.
+  def handle_info(:after_join, socket) do
+    grid = SeqState.get_grid |> grid_to_list
+    push socket, "grid", %{ grid: grid }
+    {:noreply, socket}
+  end
+
+
   def handle_in(
     "set_cell",
     %{ "x" => x, "y" => y, "active" => active },
     socket
-  ) when is_boolean(active) do
+  ) when is_boolean(active)
+  do
     grid = SeqState.set_cell(x, y, active)
-    data = grid |> Tuple.to_list |> Enum.map(&Tuple.to_list/1)
+    data = grid |> grid_to_list
     broadcast! socket, "grid", %{ grid: data }
     {:noreply, socket}
+  end
+
+
+  defp grid_to_list(grid) do
+    grid |> Tuple.to_list |> Enum.map(&Tuple.to_list/1)
   end
 end
