2b14c82e881518a4f081defe67138040c00510bd
Louis Pilfold <louis@lpil.uk>
2016-05-22 14:36:34 +0100

    Lines, yo

--- src/main.js
+++ src/main.js
@@ -1,3 +1,26 @@
 require("file?name=index.html!./index.html");
 
-console.log("Hello, world!");
+import springy         from "springy";
+import { newRenderer } from "./renderer";
+
+const graph = new springy.Graph();
+
+const node1 = graph.newNode({label: "1"});
+const node2 = graph.newNode({label: "2"});
+const node3 = graph.newNode({label: "3"});
+const node4 = graph.newNode({label: "4"});
+
+graph.newEdge(node1, node2);
+graph.newEdge(node2, node3);
+graph.newEdge(node2, node4);
+
+const layout = new springy.Layout.ForceDirected(
+  graph,
+  400.0, // Spring stiffness
+  400.0, // Node repulsion
+  0.5    // Damping
+);
+
+const canvas   = document.querySelector("canvas");
+const renderer = newRenderer(canvas, layout);
+renderer.start();
