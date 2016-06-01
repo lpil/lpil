53678b2a4e5c8f00a62bb3bf31bced7f113b086c
Louis Pilfold <louis@lpil.uk>
2016-06-01 18:17:52 +0100

    Redirect to HTTPS

--- main.js
+++ main.js
@@ -1,9 +1,17 @@
-// Email addresses in CSS have been muddled for anti-spam reasons.
-var unmuddle = function unmuddle(email) {
-  email.href = email.href.replace(/my-first-name/, 'louis');
-  email.href = email.href.replace(/this-domain/, 'lpil.uk');
-};
-var muddled_emails = document.getElementsByClassName('js-muddled-email');
-for (var i = 0, l = muddled_emails.length; i < l; i ++) {
-  unmuddle(muddled_emails[i]);
-}
+(function(window) {
+  var href = window.location.href;
+  if (href.match(/^http:\/\/lpil.uk/)) {
+    var dest = href.replace(/^http/, "https");
+    window.location = dest;
+  }
+
+  // Email addresses in CSS have been muddled for anti-spam reasons.
+  var unmuddle = function unmuddle(email) {
+    email.href = email.href.replace(/my-first-name/, 'louis');
+    email.href = email.href.replace(/this-domain/, 'lpil.uk');
+  };
+  var muddled_emails = document.getElementsByClassName('js-muddled-email');
+  for (var i = 0, l = muddled_emails.length; i < l; i ++) {
+    unmuddle(muddled_emails[i]);
+  }
+}(window));
