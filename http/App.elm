db67beaf64e8951b008a8774aabd90ccec77a0c9
Louis Pilfold <louis@lpil.uk>
2016-06-01 21:25:18 +0100

    http

---
+++ http/App.elm
@@ -0,0 +1,79 @@
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (onClick)
+import Html.App
+import Http
+import Task
+import Json.Decode
+
+main =
+  Html.App.program
+    { init = init
+    , subscriptions = subscriptions
+    , update = update
+    , view = view
+    }
+
+-- Model
+
+type alias Model =
+  { topic : String
+  , gifUrl : String
+  }
+
+init : (Model, Cmd Msg)
+init =
+  let
+    model = Model "kittens" ""
+  in
+
+  (model, getRandomGif model.topic)
+
+-- Update
+
+type Msg
+  = GifRequest
+  | FetchSucceed String
+  | FetchFail Http.Error
+
+update : Msg -> Model -> (Model, Cmd Msg)
+update msg model =
+  case msg of
+    GifRequest ->
+      (model, getRandomGif model.topic)
+
+    FetchSucceed url ->
+      ({ model | gifUrl = url }, Cmd.none)
+
+    FetchFail error ->
+      (model, Cmd.none)
+
+getRandomGif : String -> Cmd Msg
+getRandomGif topic =
+  let
+    url =
+      "http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=" ++ topic
+  in
+    Task.perform FetchFail FetchSucceed (Http.get decodeGifUrl url)
+
+decodeGifUrl : Json.Decode.Decoder String
+decodeGifUrl =
+  Json.Decode.at ["data", "image_url"] Json.Decode.string
+
+-- Subscriptions
+
+subscriptions : Model -> Sub a
+subscriptions model =
+  Sub.none
+
+-- View
+
+view : Model -> Html Msg
+view model =
+  div []
+    [ h2 [] [ text model.topic ]
+    , button [ onClick GifRequest ] [ text "More Please!" ]
+    , br [] []
+    , br [] []
+    , img [ src model.gifUrl ] []
+    ]
