ae8ec0c6eea57508699934a2fc9987d243b049c0
Louis Pilfold <louis@lpil.uk>
2016-07-10 23:39:15 +0100

    Para tcp echo server

--- lyse-sockets/lib/para_tcp.ex
+++ lyse-sockets/lib/para_tcp.ex
@@ -1,41 +1,76 @@
+defmodule ParaTCP do
+  def start(port \\ 4000) do
+    ParaTCP.Supervisor.start_link(port)
+  end
+end
+
 defmodule ParaTCP.Supervisor do
   use Supervisor
 
-  @port 4000
-  @child_args [
-  ]
-
-  def start_link do
-    Supervisor.start_link(__MODULE__, :ok, name: __MODULE__)
+  def start_link(port) do
+    Supervisor.start_link(__MODULE__, port, name: __MODULE__)
   end
 
-  def init(:ok) do
-    {:ok, listen_socket} = :gen_tcp.listen(@port, active: :once, packet: :line)
-    async_start_children()
+  def init(port) do
+    {:ok, listen_socket} = :gen_tcp.listen(port, active: :once, packet: :line)
+    start_listeners()
     children = [
-      worker(ParaTCP.Listener, [@child_args], []),
+      worker(ParaTCP.Listener, [listen_socket], []),
     ]
     supervise(children, strategy: :simple_one_for_one)
   end
 
-  defp async_start_children do
+  def start_listener do
+    Supervisor.start_child(__MODULE__, [])
+  end
+
+  defp start_listeners do
     spawn_link fn->
-      for i <- 1..20 do
-        Supervisor.start_child(__MODULE__, [])
-      end
+      for _ <- 1..20, do: start_listener()
     end
   end
 end
 
 defmodule ParaTCP.Listener do
+  require Logger
   use GenServer
 
-  def start_link(args) do
-    GenServer.start_link(__MODULE__, args)
+  def start_link(socket) do
+    GenServer.start_link(__MODULE__, socket)
   end
 
-  def init(x) do
-    IO.inspect x
-    {:ok, x}
+  def init(socket) do
+    GenServer.cast(self(), :start_accepting)
+    {:ok, socket}
+  end
+
+  def handle_cast(:start_accepting, socket) do
+    {:ok, socket} = :gen_tcp.accept(socket)
+    ParaTCP.Supervisor.start_listener()
+    tcp_send(socket, "Hi! I'm going to echo anything you send.\n")
+    {:noreply, socket}
+  end
+
+  def handle_info({:tcp, socket, 'quit' ++ _}, socket),
+    do: close(socket)
+  def handle_info({:tcp_closed, socket}, socket),
+    do: close(socket)
+
+  def handle_info({:tcp, socket, msg}, socket) do
+    Logger.info "Got message from client"
+    tcp_send(socket, msg)
+    {:noreply, socket}
+  end
+
+  defp close(socket) do
+    Logger.info "Client quit"
+    :gen_tcp.close(socket)
+    {:stop, :normal, socket}
+  end
+
+  defp tcp_send(socket, msg) do
+    :ok = :gen_tcp.send(socket, msg)
+    :ok = :inet.setopts(socket, active: :once)
+    :ok
   end
 end
