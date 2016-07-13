411a9109a7dd183e577fff828016153f51d03aa3
Louis Pilfold <louis@lpil.uk>
2016-07-13 14:04:18 +0100

    This is far better

--- rust/leap/src/lib.rs
+++ rust/leap/src/lib.rs
@@ -1,13 +1,3 @@
-// on every year that is evenly divisible by 4
-//   except every year that is evenly divisible by 100
-//     unless the year is also evenly divisible by 400
-
 pub fn is_leap_year(year: u16) -> bool {
-    is_divisible(year, 4)
-        && (!is_divisible(year, 100) || is_divisible(year, 400))
-
-}
-
-fn is_divisible(num: u16, div: u16) -> bool {
-    num % div == 0
+    (year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0)
 }
