ee2385b0455dc230e2287cbf32c14b2c8db7a8fb
Louis Pilfold <louis@lpil.uk>
2016-08-07 17:56:06 +0100

    Elixir Hello World

---
+++ elixir/hello-world/hello_world.exs
@@ -0,0 +1,29 @@
+defmodule HelloWorld do
+  @moduledoc """
+  Elixir counts the number of arguments as part of the function name.
+  For instance;
+
+      def hello() do
+      end
+
+  would be a completely different function from
+
+      def hello(name) do
+      end
+
+  Can you find a way to make all the tests pass with just one
+  function?
+
+  Hint: look into argument defaults here:
+  http://elixir-lang.org/getting-started/modules.html#default-arguments
+  """
+
+  @doc """
+  Greets the user by name, or by saying "Hello, World!"
+  if no name is given.
+  """
+  @spec hello(String.t) :: String.t
+  def hello(name \\ "World") do
+    "Hello, #{name}!"
+  end
+end
---
+++ elixir/hello-world/hello_world_test.exs
@@ -0,0 +1,23 @@
+if !System.get_env("EXERCISM_TEST_EXAMPLES") do
+  Code.load_file("hello_world.exs", __DIR__)
+end
+
+ExUnit.start
+ExUnit.configure exclude: :pending, trace: true
+
+defmodule HelloWorldTest do
+  use ExUnit.Case
+
+  test "says hello with no name" do
+    assert HelloWorld.hello() == "Hello, World!"
+  end
+
+  test "says hello sample name" do
+    assert HelloWorld.hello("Alice") == "Hello, Alice!"
+  end
+
+  test "says hello other sample name" do
+    assert HelloWorld.hello("Bob") == "Hello, Bob!"
+  end
+
+end
