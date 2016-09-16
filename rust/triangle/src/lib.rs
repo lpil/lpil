bc9d9f996a95fb6e3ec80d0e5ce97e2c20c92846
Louis Pilfold <louis@lpil.uk>
2016-09-16 23:43:42 +0100

    Rename TriangleType

--- rust/triangle/src/lib.rs
+++ rust/triangle/src/lib.rs
@@ -1,11 +1,11 @@
 #[derive(Debug,PartialEq)]
-pub enum TriangleType {
+pub enum TriangleOrder {
     Equalateral,
     Isosceles,
     Scalene,
 }
 
-pub struct Triangle(TriangleType);
+pub struct Triangle(TriangleOrder);
 
 impl Triangle {
     pub fn build(sides: [usize; 3]) -> Result<Triangle, String> {
@@ -18,27 +18,27 @@ impl Triangle {
         if sml < 1 || big >= med + sml {
             Err(String::from("Invalid sides"))
         } else if big == med && med == sml {
-            Ok(Triangle(TriangleType::Equalateral))
+            Ok(Triangle(TriangleOrder::Equalateral))
         } else if big == med || med == sml || sml == big {
-            Ok(Triangle(TriangleType::Isosceles))
+            Ok(Triangle(TriangleOrder::Isosceles))
         } else {
-            Ok(Triangle(TriangleType::Scalene))
+            Ok(Triangle(TriangleOrder::Scalene))
         }
     }
 
     pub fn is_equilateral(&self) -> bool {
-        self.order() == &TriangleType::Equalateral
+        self.order() == &TriangleOrder::Equalateral
     }
 
     pub fn is_isosceles(&self) -> bool {
-        self.order() == &TriangleType::Isosceles
+        self.order() == &TriangleOrder::Isosceles
     }
 
     pub fn is_scalene(&self) -> bool {
-        self.order() == &TriangleType::Scalene
+        self.order() == &TriangleOrder::Scalene
     }
 
-    pub fn order(&self) -> &TriangleType {
+    pub fn order(&self) -> &TriangleOrder {
         let &Triangle(ref value) = self;
         value
     }
