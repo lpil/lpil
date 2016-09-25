8c5945b4ccdf107fd983b8450136fd6d746a3231
Louis Pilfold <louis@lpil.uk>
2016-09-25 09:26:41 +0100

    Elixir rle

---
+++ elixir/run-length-encoding/rle_test.exs
@@ -0,0 +1,34 @@
+if !System.get_env("EXERCISM_TEST_EXAMPLES") do
+  Code.load_file("rle.exs", __DIR__)
+end
+
+ExUnit.start
+ExUnit.configure exclude: :pending, trace: true
+
+defmodule RunLengthEncoderTest do
+  use ExUnit.Case
+
+  test "empty string returns empty" do
+    assert RunLengthEncoder.encode("") === ""
+  end
+
+  test "simple string gets encoded" do
+    assert RunLengthEncoder.encode("AAA") === "3A"
+  end
+
+  test "more complicated string" do
+    assert RunLengthEncoder.encode("HORSE") == "1H1O1R1S1E"
+  end
+
+  test "an even more complex string" do
+    assert RunLengthEncoder.encode("WWWWWWWWWWWWBWWWWWWWWWWWWBBBWWWWWWWWWWWWWWWWWWWWWWWWB") === "12W1B12W3B24W1B"
+  end
+
+  test "it decodes an encoded simple string" do
+    assert RunLengthEncoder.decode("3A") === "AAA"
+  end
+
+  test "it decodes a more complicated string" do
+    assert RunLengthEncoder.decode("12W1B12W3B24W1B") === "WWWWWWWWWWWWBWWWWWWWWWWWWBBBWWWWWWWWWWWWWWWWWWWWWWWWB"
+  end
+end
