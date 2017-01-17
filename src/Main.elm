42ea7d3a0a69dbc26645f47609253c5d00c50e00
Louis Pilfold <louis@lpil.uk>
2017-01-17 18:31:11 +0000

    Init

---
+++ src/Main.elm
@@ -0,0 +1,8 @@
+module Main exposing (..)
+
+import Html exposing (Html)
+
+
+main : Html a
+main =
+    Html.text "Hello, world!"
---
+++ src/index.js
@@ -0,0 +1,3 @@
+var ele = document.getElementById("main");
+var app = require("./Main.elm").Main;
+app.embed(ele);
