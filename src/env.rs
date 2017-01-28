20552b8320bd4ac21e359c54a0e6d2769e571b3f
Louis Pilfold <louis@lpil.uk>
2017-01-28 18:09:39 +0000

    Symbol dereferencing

--- src/env.rs
+++ src/env.rs
@@ -41,8 +41,8 @@ impl Env {
 
     /// Get a variable from the current scope.
     ///
-    pub fn get_var(&mut self, k: String) -> Option<ast::Node> {
-        self.scope.borrow().get(&k).map(|e| e.clone())
+    pub fn get_var(&mut self, k: &String) -> Option<ast::Node> {
+        self.scope.borrow().get(k).map(|e| e.clone())
     }
 }
 
@@ -56,14 +56,14 @@ mod tests {
     fn set_and_get_var_in_env() {
         let mut env = Env::new();
         env.set_var("age".to_string(), ast::number(100.0));
-        let res = env.get_var("age".to_string());
+        let res = env.get_var(&"age".to_string());
         assert_eq!(res, Some(ast::number(100.0)));
     }
 
     #[test]
     fn get_var_when_unset() {
         let mut env = Env::new();
-        let res = env.get_var("santa".to_string());
+        let res = env.get_var(&"santa".to_string());
         assert_eq!(res, None);
     }
 }
