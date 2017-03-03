caf2356873d962f510203662868fab42a6b2d62d
Louis Pilfold <louis@lpil.uk>
2017-03-03 16:13:13 +0000

    Bump buttons to latest elm

--- buttons/App.elm
+++ buttons/App.elm
@@ -1,24 +1,34 @@
-import Html exposing (div, button, text)
-import Html.App exposing (beginnerProgram)
+module Main exposing (..)
+
+import Html exposing (beginnerProgram, div, button, text)
 import Html.Events exposing (onClick)
 
-type Msg = Increment | Decrement
+
+type Msg
+    = Increment
+    | Decrement
+
 
 main =
-  beginnerProgram
-    { model = 0
-    , view = view
-    , update = update }
+    beginnerProgram
+        { model = 0
+        , view = view
+        , update = update
+        }
 
-view model = div []
-  [ button [ onClick Decrement ] [ text "-" ]
-  , div [] [model |> toString |> text]
-  , button [ onClick Increment ] [ text "+" ]
-  ]
+
+view model =
+    div []
+        [ button [ onClick Decrement ] [ text "-" ]
+        , div [] [ model |> toString |> text ]
+        , button [ onClick Increment ] [ text "+" ]
+        ]
+
 
 update msg model =
-  case msg of
-    Increment ->
-      model + 1
-    Decrement ->
-      model - 1
+    case msg of
+        Increment ->
+            model + 1
+
+        Decrement ->
+            model - 1
