b7dc9b6ce44c353dfb87dd158110113883091180
Louis Pilfold <louis@lpil.uk>
2017-03-11 18:40:35 +0000

    Run prettier on webpack config files

--- webpack.config.js
+++ webpack.config.js
@@ -2,25 +2,24 @@ const path = require("path");
 const webpack = require("webpack");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-const merge             = require("webpack-merge");
+const merge = require("webpack-merge");
 
 module.exports = {
   output: {
-    path:     path.resolve(__dirname, "dist/"),
-    filename: "[hash].js",
+    path: path.resolve(__dirname, "dist/"),
+    filename: "[hash].js"
   },
 
   module: {
     noParse: /\.elm$/,
-    loaders: [
-    ]
+    loaders: []
   },
 
   plugins: [
     new HtmlWebpackPlugin({
       template: "src/index.html",
-      inject:   "body",
+      inject: "body",
       filename: "index.html"
-    }),
-  ],
-}
+    })
+  ]
+};
--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -3,23 +3,22 @@ const merge = require("webpack-merge");
 const config = require("./webpack.config");
 
 module.exports = merge(config, {
-
   entry: [
     "webpack-dev-server/client?http://localhost:8080",
     path.join(__dirname, "src/index.js")
   ],
 
   devServer: {
-    inline: true,
+    inline: true
   },
 
   module: {
     loaders: [
       {
-        test:    /\.elm$/,
+        test: /\.elm$/,
         exclude: [/elm-stuff/, /node_modules/],
-        loader:  "elm-hot-loader!elm-webpack-loader?debug=true&verbose=true&warn=true"
-      },
-    ],
-  },
+        loader: "elm-hot-loader!elm-webpack-loader?debug=true&verbose=true&warn=true"
+      }
+    ]
+  }
 });
