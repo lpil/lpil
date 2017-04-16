602fe2469097a7a0b78e2128ffc7c2d7becece40
Louis Pilfold <louis@lpil.uk>
2017-04-16 23:20:21 +0100

    I clearly know how parser combinators work

--- src/Xcss/Parser.hs
+++ src/Xcss/Parser.hs
@@ -9,16 +9,16 @@ module Xcss.Parser
 
 import Control.Applicative (empty)
 import Control.Monad (void)
+import Data.Maybe (catMaybes)
 import Text.Megaparsec
 import Text.Megaparsec.Expr
 import qualified Text.Megaparsec.Lexer as L
-import Text.Megaparsec.String -- input stream is of type ‘String’
+import Text.Megaparsec.Text
 
 type CssParseError = ParseError Char Text.Megaparsec.Dec
 
-data Statement
-  = Statements [Statement]
-  | Block String
+data Statement =
+  Function
   deriving (Show, Eq)
 
 -- | Parse CSS into AST
@@ -28,34 +28,90 @@ parseCss = parse cssParser
 
 -- | CSS Parser
 -- |
-cssParser :: Parser Statement
-cssParser = between whitespace eof topStatements
+cssParser :: Parser [Statement]
+cssParser = do
+  statements <- between whitespace eof topStatements
+  pure $ catMaybes statements
 
 -- | Parse a sequence of statements
 -- |
-topStatements :: Parser Statement
-topStatements = Statements <$> manyTill topStatement eof
+topStatements :: Parser [Maybe Statement]
+topStatements = manyTill topStatement eof
 
 -- | Parse a sequence of statements seperated by semicolons.
 -- |
-topStatement :: Parser Statement
+topStatement :: Parser (Maybe Statement)
 topStatement = blockStatement
 
 -- | Rule block
 -- |
 -- | div { ... }
 -- |
-blockStatement :: Parser Statement
+blockStatement :: Parser (Maybe Statement)
 blockStatement = do
-  selector <- identifier
+  selector
   rword "{"
+  ruleList
   rword "}"
-  return $ Block selector
+  return Nothing
 
--- | Parse an identifier word
+-- | Parse a list of rules
 -- |
-identifier :: Parser String
-identifier = (lexeme . try) $ (:) <$> letterChar <*> many alphaNumChar
+ruleList :: Parser [()]
+ruleList = lexeme $ sepEndBy rule (rword ";")
+
+-- | Parse a rule
+-- |
+rule :: Parser ()
+rule = do
+  ruleIdentifier
+  rword ":"
+  ruleValue
+  pure ()
+
+-- | Parse an identifier word used in a selector
+-- |
+ruleIdentifier :: Parser String
+ruleIdentifier = (lexeme . try) $ (:) <$> startChar <*> many restChar
+  where
+    punct = char '-' <|> char '_'
+    startChar = letterChar <|> punct
+    restChar = alphaNumChar <|> punct
+
+-- | Parse a string
+-- |
+singleQuoteString :: Parser String
+singleQuoteString = do
+  char '\''
+  content <- manyTill anyChar (char '\'')
+  pure content
+
+-- | Parse a string
+-- |
+doubleQuoteString :: Parser String
+doubleQuoteString = do
+  char '"'
+  content <- manyTill anyChar (char '"')
+  pure content
+
+-- | Parse a rule's value
+-- |
+ruleValue :: Parser String
+ruleValue = singleQuoteString <|> doubleQuoteString<|> lexeme (many alphaNumChar)
+
+-- | Parse an identifier word used in a selector
+-- |
+selector :: Parser [String]
+selector = many selectorIdentifier
+
+-- | Parse an identifier word used in a selector
+-- |
+selectorIdentifier :: Parser String
+selectorIdentifier = (lexeme . try) $ (:) <$> startChar <*> many restChar
+  where
+    spacer = char '-' <|> char '_'
+    startChar = letterChar <|> char '#' <|> char '.'
+    restChar = alphaNumChar <|> spacer
 
 -- | Parser a reserved word/symbol
 -- |
@@ -90,8 +146,3 @@ parens = between (symbol "(") (symbol ")")
 -- |
 integer :: Parser Integer
 integer = lexeme L.integer
-
--- | Parse a semicolon.
--- |
-semi :: Parser String
-semi = symbol ";"
