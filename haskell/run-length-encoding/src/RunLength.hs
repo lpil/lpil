27c8b68df44e555c7a25656c70685ec14830ec9a
Louis Pilfold <louis@lpil.uk>
2017-06-04 19:13:27 +0100

    Haskell run length encoding

---
+++ haskell/run-length-encoding/src/RunLength.hs
@@ -0,0 +1,25 @@
+module RunLength
+  ( decode
+  , encode
+  ) where
+
+import Data.Char (isDigit)
+import Data.List (group, take)
+
+decode :: String -> String
+decode "" = ""
+decode [x] = [x]
+decode chars@(first:rest)
+  | (not . isDigit) first = first : decode rest
+  | otherwise =
+    let size = (read . takeWhile isDigit) chars
+        char:rest = dropWhile isDigit chars
+    in replicate size char ++ decode rest
+
+encode :: String -> String
+encode = concatMap encodeRun . group
+  where
+    encodeRun :: String -> String
+    encodeRun [] = []
+    encodeRun [x] = [x]
+    encodeRun xs = (show . length) xs ++ take 1 xs
