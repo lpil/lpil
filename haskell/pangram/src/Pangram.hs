6cba7ef713a0fbac55c0dbda3f4e0510b341a16a
Louis Pilfold <louis@lpil.uk>
2017-06-04 17:33:23 +0100

    Haskell Pangram

---
+++ haskell/pangram/src/Pangram.hs
@@ -0,0 +1,13 @@
+module Pangram
+  ( isPangram
+  ) where
+
+import Data.Char (toLower)
+import Data.Set (fromList, isSubsetOf)
+
+isPangram :: String -> Bool
+isPangram text = isSubsetOf alphabet (lowerSet text)
+  where
+    lowerSet = fromList . toLower'
+    toLower' = map toLower
+    alphabet = fromList "abcdefghijklmnopqrstuvwxyz"
