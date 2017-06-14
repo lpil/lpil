3e12e680db03c0bb5a29e2428de53bd22b9b6018
Louis Pilfold <louis@lpil.uk>
2017-06-15 00:19:59 +0100

    Haskell raindrops

---
+++ haskell/raindrops/src/Raindrops.hs
@@ -0,0 +1,14 @@
+module Raindrops
+  ( convert
+  ) where
+
+import Data.Maybe (fromMaybe)
+
+convert :: Int -> String
+convert n =
+  fromMaybe (show n) $
+  mconcat [sound 3 "Pling", sound 5 "Plang", sound 7 "Plong"]
+  where
+    sound div noise
+      | mod n div == 0 = Just noise
+      | otherwise = Nothing
