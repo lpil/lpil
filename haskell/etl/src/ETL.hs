104734b2c21af73aafb82b9ef5a5e62ad3326613
Louis Pilfold <louis@lpil.uk>
2017-06-14 23:45:46 +0100

    Haskell etl

---
+++ haskell/etl/src/ETL.hs
@@ -0,0 +1,13 @@
+module ETL
+  ( transform
+  ) where
+
+import Control.Category ((>>>))
+import Data.Char (toLower)
+import Data.Map (Map, toList, fromList)
+
+transform :: Map a String -> Map Char a
+transform = toList >>> concatMap transformPair >>> fromList
+  where
+    transformPair (score, letters) = map (tuple score) letters
+    tuple score letter = (toLower letter, score)
