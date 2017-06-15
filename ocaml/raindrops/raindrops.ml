8f99d819ad77958f82d5d03edef9b36a4347d0b3
Louis Pilfold <louis@lpil.uk>
2017-06-15 18:04:20 +0100

    Ocaml raindrops

---
+++ ocaml/raindrops/raindrops.ml
@@ -0,0 +1,14 @@
+open Core
+
+let sound n div str =
+  match n mod div with
+  | 0 -> str
+  | _ -> ""
+
+let raindrop n =
+  let sounds = sound n 3 "Pling" ^
+               sound n 5 "Plang" ^
+               sound n 7 "Plong" in
+  match sounds with
+  | "" -> string_of_int n
+  | _ -> sounds
