64346e43cca4b6ab39a9d05c0a890869dc396ca3
Louis Pilfold <louis@lpil.uk>
2017-08-05 00:40:36 +0100

    Begin parsing

--- test/Main.purs
+++ test/Main.purs
@@ -1,6 +1,6 @@
 module Test.Main where
 
-import Prelude (Unit, discard, ($), (+), (==))
+import Prelude (Unit, ($), discard)
 import Test.Unit (suite, test)
 import Test.Unit.Main (runTest)
 import Test.Unit.Assert as Assert
@@ -8,6 +8,8 @@ import Test.Unit.Console (TESTOUTPUT)
 import Control.Monad.Eff (Eff)
 import Control.Monad.Aff.AVar (AVAR)
 import Control.Monad.Eff.Console (CONSOLE)
+import Data.Either (Either(..))
+import Data.Stylesheet (Ast(..), parse)
 
 main :: forall e. Eff
   ( console :: CONSOLE
@@ -16,9 +18,17 @@ main :: forall e. Eff
   | e
   ) Unit
 main = runTest do
-  suite "sync code" do
-    test "arithmetic" do
-      Assert.assert "2 + 2 should be 4" $ (2 + 2) == 4
-      Assert.assertFalse "2 + 2 shouldn't be 5" $ (2 + 2) == 5
-      Assert.equal 4 (2 + 2)
-      Assert.expectFailure "2 + 2 shouldn't be 5" $ Assert.equal 5 (2 + 2)
+  suite "Data.Stylesheet" do
+    suite "parse" do
+      test "blank source" do
+        let source = ""
+        Assert.equal (parse source) $ Left []
+
+      {-- test "empty function" do --}
+      {--   let source = """ --}
+      {--   @function main { --}
+      {--     return: "Hello, world!"; --}
+      {--   } --}
+      {--   """ --}
+      {--   let nodes = [ Function { name: "main" } ] --}
+      {--   Assert.equal (parse source) (Left nodes) --}
