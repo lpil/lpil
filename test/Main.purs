2e5e996c6d1ece5980e974dc3fc7d482c4be3a73
Louis Pilfold <louis@lpil.uk>
2017-08-04 21:54:27 +0100

    Add TestUnit

--- test/Main.purs
+++ test/Main.purs
@@ -1,9 +1,24 @@
 module Test.Main where
 
-import Prelude
+import Prelude (Unit, discard, ($), (+), (==))
+import Test.Unit (suite, test)
+import Test.Unit.Main (runTest)
+import Test.Unit.Assert as Assert
+import Test.Unit.Console (TESTOUTPUT)
 import Control.Monad.Eff (Eff)
-import Control.Monad.Eff.Console (CONSOLE, log)
+import Control.Monad.Aff.AVar (AVAR)
+import Control.Monad.Eff.Console (CONSOLE)
 
-main :: forall e. Eff (console :: CONSOLE | e) Unit
-main = do
-  log "You should add some tests."
+main :: forall e. Eff
+  ( console :: CONSOLE
+  , testOutput :: TESTOUTPUT
+  , avar :: AVAR
+  | e
+  ) Unit
+main = runTest do
+  suite "sync code" do
+    test "arithmetic" do
+      Assert.assert "2 + 2 should be 4" $ (2 + 2) == 4
+      Assert.assertFalse "2 + 2 shouldn't be 5" $ (2 + 2) == 5
+      Assert.equal 4 (2 + 2)
+      Assert.expectFailure "2 + 2 shouldn't be 5" $ Assert.equal 5 (2 + 2)
