7f447fdd2466daa11cfd17084bc8c626ace75633
Louis Pilfold <louis@lpil.uk>
2018-01-23 13:06:39 +0000

    ocaml nucleotide count

---
+++ ocaml/nucleotide-count/nucleotide_count.ml
@@ -0,0 +1,10 @@
+open Core
+
+let count str c =
+  str |> String.to_list_rev |> List.filter ~f:(( == ) c) |> List.length
+
+
+let nucleotide_counts str =
+  let inc x = x |> Option.value ~default:0 |> ( + ) 1 in
+  let inc_counter = Char.Map.update ~f:inc in
+  str |> String.to_list_rev |> List.fold ~init:Char.Map.empty ~f:inc_counter
