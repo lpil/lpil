b8ca586a8e29aec9847db13720a7df756f7c33fb
Louis Pilfold <louis@lpil.uk>
2018-05-09 20:00:06 +0100

    API serves presigned URLs

--- src/server.js
+++ src/server.js
@@ -1,8 +1,45 @@
 import express from "express";
-import Storage from "@google-cloud/storage";
+import * as storage from "./storage";
 
 const server = express();
 
-server.get("/", (req, res) => res.send("Hello World!"));
+const handleError = res => error => {
+  switch (error.type) {
+    case "gallery-not-found":
+      console.log("gallery not found");
+      return res.status(404).json({ error: "gallery not found" });
+
+    case "invalid-password":
+      console.log("invalid password");
+      return res.status(400).json({ error: "invalid password" });
+
+    default:
+      console.error(error);
+      res.status(500).json({ error: "internal server error" });
+  }
+};
+
+const checkPassword = password => passwords => {
+  if (!passwords.includes(password)) {
+    throw { type: "invalid-password" };
+  }
+};
+
+server.get("/", (req, res) => res.send("Hello, mum!"));
+
+server.get("/gallery/:id", (req, res) => {
+  const password = req.query.password;
+  const galleryId = req.params.id;
+
+  storage
+    .getGalleryPasswords(galleryId)
+    .then(checkPassword(password))
+    .then(_ => storage.getGalleryImages(galleryId))
+    .then(storage.getSignedUrls)
+    .then(images => {
+      res.json({ images });
+    })
+    .catch(handleError(res));
+});
 
 export default server;
