dda2ddb55b0fabdf0aaaa9c9dc410390d43fb26f
Louis Pilfold <louis@lpil.uk>
2018-07-20 17:44:25 +0100

    stdout audio

--- src/main.rs
+++ src/main.rs
@@ -1,54 +1,29 @@
-extern crate cpal;
+//! Outputs a 16 bit little endian 44100 sine wave over stdout.
+//!
+//! Pipe into `aplay -f cd` on Linux to run.
+//!
+extern crate byteorder;
+
+use byteorder::{LittleEndian, WriteBytesExt};
+use std::io::{self, Write};
+
+const MAX_16BIT_SIGNED_INT_AS_FLOAT: f32 = 32767.0;
+const SAMPLE_RATE: f32 = 44100.0;
+const TWO_PI: f32 = 3.141592 * 2.0;
 
 fn main() {
-    let device = cpal::default_output_device().expect("Failed to get default output device");
-    let format = device
-        .default_output_format()
-        .expect("Failed to get default output format");
-    let event_loop = cpal::EventLoop::new();
-    let stream_id = event_loop.build_output_stream(&device, &format).unwrap();
-    event_loop.play_stream(stream_id.clone());
-
-    let sample_rate = format.sample_rate.0 as f32;
     let mut sample_clock = 0f32;
 
-    // Produce a sinusoid of maximum amplitude.
-    let mut next_value = || {
-        sample_clock = (sample_clock + 1.0) % sample_rate;
-        (sample_clock * 440.0 * 2.0 * 3.141592 / sample_rate).sin()
+    let mut next_sample = || {
+        sample_clock = (sample_clock + 1.0) % SAMPLE_RATE;
+        let sample = (sample_clock * 110.0 * TWO_PI / SAMPLE_RATE).sin();
+        let scaled_sample = sample * MAX_16BIT_SIGNED_INT_AS_FLOAT;
+        scaled_sample
     };
 
-    event_loop.run(move |_, data| match data {
-        cpal::StreamData::Output {
-            buffer: cpal::UnknownTypeOutputBuffer::U16(mut buffer),
-        } => {
-            for sample in buffer.chunks_mut(format.channels as usize) {
-                let value = ((next_value() * 0.5 + 0.5) * std::u16::MAX as f32) as u16;
-                for out in sample.iter_mut() {
-                    *out = value;
-                }
-            }
-        }
-        cpal::StreamData::Output {
-            buffer: cpal::UnknownTypeOutputBuffer::I16(mut buffer),
-        } => {
-            for sample in buffer.chunks_mut(format.channels as usize) {
-                let value = (next_value() * std::i16::MAX as f32) as i16;
-                for out in sample.iter_mut() {
-                    *out = value;
-                }
-            }
-        }
-        cpal::StreamData::Output {
-            buffer: cpal::UnknownTypeOutputBuffer::F32(mut buffer),
-        } => {
-            for sample in buffer.chunks_mut(format.channels as usize) {
-                let value = next_value();
-                for out in sample.iter_mut() {
-                    *out = value;
-                }
-            }
-        }
-        _input => (),
-    });
+    loop {
+        io::stdout()
+            .write_i16::<LittleEndian>(next_sample() as i16)
+            .unwrap();
+    }
 }
