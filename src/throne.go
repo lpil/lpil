e3800c3f798bf17c8d86ce7a775dd5353be90dcf
Harry Fox <harryfox89@gmail.com>
2018-07-26 15:48:00 +0100

    Write buffer in advance

--- src/throne.go
+++ src/throne.go
@@ -7,26 +7,35 @@ import (
 )
 
 const Max16BitSignedIntAsFloat = 32767.0
+
 const SampleRate = 44100.0
 const TwoPi = 3.141592 * 2.0 // One full rotation of a sine wave
 const SampleStep = TwoPi / SampleRate
 
+const Channels = 2
+const BytesInSample = 2
+
 var frequency = 110.0
-var sampleClock = 0.0
+var phase = 0.0
 
 func main() {
-	// need some working memory to turn our sine output into binary every
-	// moment-needs to be 16 bits 2 bytes long
-	buffer := make([]byte, 2)
+	numSamples := int(SampleRate / frequency)
+	numBytesInBuffer := numSamples * Channels * BytesInSample
+	buffer := make([]byte, numBytesInBuffer)
 
-	// by continuously printing nextsample, we produce a stream of data
-	// approximating a sine wave make voodoo float number into integer
-	// for makes a function loop forever
-	for {
+	i := 0
+	for i < numBytesInBuffer {
 		sample := int16(nextSample())
 
-		// write that integer into binary for aplay to use
-		binary.LittleEndian.PutUint16(buffer[0:], uint16(sample))
+		// Write left channel sample
+		binary.LittleEndian.PutUint16(buffer[i:], uint16(sample))
+		// Write right channel sample
+		binary.LittleEndian.PutUint16(buffer[i+BytesInSample:], uint16(sample))
+
+		i = i + 4
+	}
+
+	for {
 		binary.Write(os.Stdout, binary.LittleEndian, buffer)
 	}
 }
@@ -35,7 +44,8 @@ func main() {
 // of sine later define by sample clock.
 // float64 is the memory format used to record the output of sine
 func nextSample() float64 {
-	sampleClock = math.Mod((sampleClock + 1.0), SampleRate)
-	sample := math.Sin(sampleClock * frequency * SampleStep)
+	sample := math.Sin(phase * TwoPi)
+	// phase = math.Mod((phase + frequency/SampleRate), TwoPi)
+	phase = phase + frequency/SampleRate
 	return sample * Max16BitSignedIntAsFloat
 }
