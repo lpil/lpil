69486704a0cf9bfb30da42c7b8866be6da79e04a
Louis Pilfold <louis@lpil.uk>
2019-08-09 16:54:55 +0100

    nix gc

--- nix/nix-ssh-deploy/src/configuration.nix
+++ nix/nix-ssh-deploy/src/configuration.nix
@@ -13,10 +13,18 @@
   networking.hostName = "purple";
   networking.firewall.allowPing = true;
   networking.firewall.allowedTCPPorts = [ 80 8080 443 ];
+  nix.gc.automatic = true;
+  nix.gc.options = "--delete-older-than 13d";
+  virtualisation.docker.enable = true;
+
+  docker-containers.happylabs-api.image = "lpil/happylabs-api-rust:11";
+  docker-containers.happylabs-api.ports = ["3000:3000"];
 
   # Traefik reverse proxy
   services.traefik.enable = true;
   services.traefik.configFile = "/etc/traefik/traefik.toml";
+
+  # Requires `$ systemctl restart traefik` to be read if changed
   environment.etc."traefik/traefik.toml".text = ''
 [entryPoints]
   [entryPoints.http]
@@ -29,6 +37,7 @@ watch = true
 filename = "/etc/traefik/watched_config.toml"
 '';
 
+  # Will be automatically reloaded by traefik if changed
   environment.etc."traefik/watched_config.toml".text = ''
 [backends]
   [backends.happylabs-api]
@@ -45,12 +54,4 @@ filename = "/etc/traefik/watched_config.toml"
       [frontends.happylabs-api.routes.route0]
         rule = "Host:happylabs-api-purple.lpil.uk"
   '';
-
-  virtualisation.docker.enable = true;
-
-  docker-containers.happylabs-api = {
-    image = "lpil/happylabs-api-rust:11";
-    ports = ["3000:3000"];
-    cmd = ["-text='Happylabs'"];
-  };
 }
