d25bba757abc949f226c68b61f944bd9e9f80194
Louis Pilfold <louis@lpil.uk>
2020-02-10 15:19:19 +0000

    Relax typing

--- ruby/sorbet/ice_cream/test/ice_cream_test.rb
+++ ruby/sorbet/ice_cream/test/ice_cream_test.rb
@@ -1,15 +1,11 @@
-# typed: strong
-require "test_helper"
+# typed: true
+require 'test_helper'
 
 class IceCreamTest < Minitest::Test
-  extend T::Sig
-
-  sig { returns(T::Boolean) }
   def test_that_it_has_a_version_number
     refute_nil ::IceCream::VERSION
   end
 
-  sig { returns(T::Boolean) }
   def test_it_does_something_useful
     assert false
   end
