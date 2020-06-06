d6cd3e307371753cdf3b55d6f9dd1e6e16a36f86
Louis Pilfold <louis@lpil.uk>
2020-06-06 12:59:15 +0100

    buffalo auth plugin

--- go/buffalo-tutorial/actions/home_test.go
+++ go/buffalo-tutorial/actions/home_test.go
@@ -1,10 +1,30 @@
 package actions
 
-import "net/http"
+import "coke/models"
 
 func (as *ActionSuite) Test_HomeHandler() {
 	res := as.HTML("/").Get()
+	as.Equal(302, res.Code)
+	as.Equal(res.Location(), "/auth/new")
+}
 
-	as.Equal(http.StatusOK, res.Code)
-	as.Contains(res.Body.String(), "Welcome to Buffalo")
+func (as *ActionSuite) Test_HomeHandler_LoggedIn() {
+	u := &models.User{
+		Email:                "mark@example.com",
+		Password:             "password",
+		PasswordConfirmation: "password",
+	}
+	verrs, err := u.Create(as.DB)
+	as.NoError(err)
+	as.False(verrs.HasAny())
+	as.Session.Set("current_user_id", u.ID)
+
+	res := as.HTML("/auth").Get()
+	as.Equal(200, res.Code)
+	as.Contains(res.Body.String(), "Sign Out")
+
+	as.Session.Clear()
+	res = as.HTML("/auth").Get()
+	as.Equal(200, res.Code)
+	as.Contains(res.Body.String(), "Sign In")
 }
--- go/buffalo-tutorial/go.mod
+++ go/buffalo-tutorial/go.mod
@@ -6,11 +6,17 @@ require (
 	github.com/gobuffalo/buffalo v0.15.5
 	github.com/gobuffalo/buffalo-pop/v2 v2.0.6
 	github.com/gobuffalo/envy v1.9.0
-	github.com/gobuffalo/mw-csrf v0.0.0-20190129204204-25460a055517
+	github.com/gobuffalo/mw-csrf v1.0.0
 	github.com/gobuffalo/mw-forcessl v0.0.0-20180802152810-73921ae7a130
 	github.com/gobuffalo/mw-i18n v0.0.0-20190129204410-552713a3ebb4
 	github.com/gobuffalo/mw-paramlogger v0.0.0-20190129202837-395da1998525
 	github.com/gobuffalo/packr/v2 v2.8.0
 	github.com/gobuffalo/pop/v5 v5.1.1
+	github.com/gobuffalo/suite/v3 v3.0.0
+	github.com/gobuffalo/validate/v3 v3.1.0
+	github.com/gofrs/uuid v3.2.0+incompatible
+	github.com/markbates/grift v1.5.0
+	github.com/pkg/errors v0.9.1
 	github.com/unrolled/secure v0.0.0-20190103195806-76e6d4e9b90c
+	golang.org/x/crypto v0.0.0-20200320181102-891825fb96df
 )
