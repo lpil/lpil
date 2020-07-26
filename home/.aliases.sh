4847046fb381e8b5a6aa646558fa98ad3da17ba9
Louis Pilfold <louis@lpil.uk>
2020-07-26 11:38:20 +0100

    mariadb

--- home/.aliases.sh
+++ home/.aliases.sh
@@ -57,9 +57,14 @@ alias sorbet-ignored="rg typed:\ ignore | fpp"
 # Run things in containers
 alias get-docker-couchdb="docker run -t -d -p 5984:5984 --name couchdb klaemo/couchdb"
 alias get-docker-elasticsearch-6.6="docker run -d --name elasticsearch-6.6 -p 9200:9200 -p 9300:9300 -e 'discovery.type=single-node' elasticsearch:6.6.1"
+
 alias get-docker-mysql-5.7="docker run -t -d -p 3306:3306 --name mysql-5.7 -e MYSQL_ALLOW_EMPTY_PASSWORD=1 mysql:5.7 --bind-address=0.0.0.0"
+alias get-docker-mariadb="docker run -t -d -p 3306:3306 --name mariadb -e MYSQL_ALLOW_EMPTY_PASSWORD=1 mariadb --bind-address=0.0.0.0"
+
 alias get-docker-neo4j="docker run -t -d -e NEO4J_AUTH=none -p 7474:7474 -p 7473:7473 -p 7687:7687  --name neo4j neo4j"
 alias get-docker-orientdb="docker run -e ORIENTDB_ROOT_PASSWORD=orientdb -t -d -p 2424:2424 -p 2480:2480 --name orientdb orientdb"
+
 alias get-docker-postgres-9.4="docker run -t -d -p 5432:5432 --name postgres-9-4 postgres:9.4.15"
 alias get-docker-postgres="docker run -t -d -p 5432:5432 --name postgres postgres"
+
 alias get-docker-redis="docker run -t -d -p 6379:6379 --name redis redis --bind 0.0.0.0"
