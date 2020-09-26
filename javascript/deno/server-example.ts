43ec983a72089b7e43f3377368e4711a7e719924
Louis Pilfold <louis@lpil.uk>
2020-09-26 20:25:11 +0100

    It does now

--- javascript/deno/server-example.ts
+++ javascript/deno/server-example.ts
@@ -1,7 +1,5 @@
 #!/usr/bin/env -S deno run --allow-net
 
-// This shebang doesn't work on Linux :(
-
 import { serve } from "https://deno.land/std@0.71.0/http/server.ts";
 
 const s = serve({ port: 8000 });
