c61a4aaf8d7c3cbb063e074335abdc66ebf5f10a
Louis Pilfold <louis@lpil.uk>
2023-01-11 20:22:31 +0000

    Add Gleam frontend to Phoenix app

--- learning/gleam/phoenix_gleam_frontend/mix.exs
+++ learning/gleam/phoenix_gleam_frontend/mix.exs
@@ -47,7 +47,8 @@ defmodule MyApp.MixProject do
       {:telemetry_poller, "~> 1.0"},
       {:gettext, "~> 0.18"},
       {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"}
+      {:plug_cowboy, "~> 2.5"},
+      {:file_system, "~> 0.2", only: :dev}
     ]
   end
 
