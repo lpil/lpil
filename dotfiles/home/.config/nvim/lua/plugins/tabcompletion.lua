b8aca1fca8497c64434f019a1d11cfb6084ebb99
Louis Pilfold <louis@lpil.uk>
2024-01-10 11:58:36 +0000

    Surround!

--- dotfiles/home/.config/nvim/lua/plugins/tabcompletion.lua
+++ dotfiles/home/.config/nvim/lua/plugins/tabcompletion.lua
@@ -7,6 +7,7 @@ return {
       return {}
     end,
   },
+
   -- then: setup supertab in cmp
   {
     "hrsh7th/nvim-cmp",
@@ -18,7 +19,12 @@ return {
       local has_words_before = function()
         unpack = unpack or table.unpack
         local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-        return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+        return col ~= 0
+          and vim.api
+              .nvim_buf_get_lines(0, line - 1, line, true)[1]
+              :sub(col, col)
+              :match("%s")
+            == nil
       end
 
       local luasnip = require("luasnip")
