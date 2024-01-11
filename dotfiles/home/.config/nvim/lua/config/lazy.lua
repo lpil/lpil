08257c36b0d642099ee7239356a3bf0e8a2edd31
Louis Pilfold <louis@lpil.uk>
2024-01-11 18:25:07 +0000

    Themes

--- dotfiles/home/.config/nvim/lua/config/lazy.lua
+++ dotfiles/home/.config/nvim/lua/config/lazy.lua
@@ -9,7 +9,16 @@ vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
 require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
-    { "LazyVim/LazyVim", import = "lazyvim.plugins" },
+    {
+      "LazyVim/LazyVim",
+      import = "lazyvim.plugins",
+      opts = {
+        -- colorscheme = "catppuccin-macchiato",
+        -- colorscheme = "onedark",
+        colorscheme = "tokyonight",
+        -- colorscheme = "sonokai",
+      },
+    },
     -- import any extras modules here
     -- { import = "lazyvim.plugins.extras.lang.typescript" },
     -- { import = "lazyvim.plugins.extras.lang.json" },
---
+++ dotfiles/home/.config/nvim/lua/plugins/themes.lua
@@ -0,0 +1,10 @@
+-- https://vimcolorschemes.com/
+--
+-- Pick the default in lazy.lua
+--
+return {
+  { "catppuccin/nvim" },
+  { "folke/tokyonight.nvim" },
+  { "navarasu/onedark.nvim" },
+  { "sainnhe/sonokai" },
+}
