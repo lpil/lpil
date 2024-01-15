8da8331fa6ab1cc428f3250c1c143ec0243078c8
Louis Pilfold <louis@lpil.uk>
2024-01-15 13:07:50 +0000

    Nicer buffer navigation

--- dotfiles/home/.config/nvim/lua/config/keymaps.lua
+++ dotfiles/home/.config/nvim/lua/config/keymaps.lua
@@ -12,3 +12,39 @@ vim.keymap.del("i", "<A-j>")
 vim.keymap.del("i", "<A-k>")
 vim.keymap.del("v", "<A-j>")
 vim.keymap.del("v", "<A-k>")
+
+--
+-- Replace bnext and bprevious with a version that skips over buffers that are
+-- open in other windows.
+--
+
+local function next_non_open_buffer(direction)
+  local initial = vim.fn.bufnr("%")
+
+  while true do
+    -- Move to the next buffer
+    vim.cmd(direction)
+    local buffer_number = vim.fn.bufnr("%")
+
+    local windows = vim.fn.win_findbuf(buffer_number)
+    local open_twice = #windows > 1
+    if not open_twice then
+      -- We've found a buffer that is not already open. Victory!
+      break
+    end
+
+    if buffer_number == initial then
+      -- We've looped around to the initial buffer, meaning there are no
+      -- buffers that are not already open. Give up.
+      break
+    end
+  end
+end
+
+vim.keymap.set("n", "L", function()
+  next_non_open_buffer("bnext")
+end)
+
+vim.keymap.set("n", "H", function()
+  next_non_open_buffer("bprevious")
+end)
