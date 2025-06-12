31cf5bb1e4e3863ab1101b7037fd7e7178c220d5
Louis Pilfold <louis@lpil.uk>
2025-06-12 11:38:11 +0100

    Markdown wrapping

---
+++ dotfiles/home/.config/nvim/after/ftplugin/markdown.lua
@@ -0,0 +1,9 @@
+-- Enable visual wrap
+vim.opt_local.wrap = true
+
+-- Wrap at word boundaries, not mid-word
+vim.opt_local.linebreak = true
+
+-- Indent wrapped lines to match the start of the original line
+vim.opt_local.breakindent = true
+vim.opt_local.showbreak = "↪ "
