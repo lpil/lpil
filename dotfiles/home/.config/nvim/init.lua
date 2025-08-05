3100c162d21371c47928ef73efe663f1395eca02
Louis Pilfold <louis@lpil.uk>
2025-08-05 21:05:42 +0100

    Trim whitespace automatically

--- dotfiles/home/.config/nvim/init.lua
+++ dotfiles/home/.config/nvim/init.lua
@@ -489,6 +489,7 @@ require("lazy").setup({
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
         javascript = { "prettierd", "prettier", stop_after_first = true },
+        ["_"] = { "trim_whitespace" },
       },
     },
   },
