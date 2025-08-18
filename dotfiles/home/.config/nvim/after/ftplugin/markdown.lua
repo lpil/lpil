96b89883b48b559f1fa40b50073911d7ad1dfdf4
Louis Pilfold <louis@lpil.uk>
2025-08-18 13:34:34 +0100

    Override vim markdown config

--- dotfiles/home/.config/nvim/after/ftplugin/markdown.lua
+++ dotfiles/home/.config/nvim/after/ftplugin/markdown.lua
@@ -7,3 +7,8 @@ vim.opt_local.linebreak = true
 -- Indent wrapped lines to match the start of the original line
 vim.opt_local.breakindent = true
 vim.opt_local.showbreak = "↪ "
+
+-- 2 space indentation
+vim.opt_local.shiftwidth = 2
+vim.opt_local.tabstop = 2
+vim.opt_local.softtabstop = 2
