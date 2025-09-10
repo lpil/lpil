23f8a5e416e673b3d36cc657aa767d72c8f0d863
Louis Pilfold <louis@lpil.uk>
2025-09-10 12:11:24 +0100

    Bike ride base

--- obsidian-plugin/main.js
+++ obsidian-plugin/main.js
@@ -15,6 +15,9 @@ class LpilPlugin extends Plugin {
 
     if (file.path.startsWith("exercise/log/circuits/"))
       return this.onCircuitChanged(file);
+
+    if (file.path.startsWith("exercise/log/bike rides/"))
+      return this.onBikeRideChanged(file);
   }
 
   async onSessionChanged(file) {
@@ -52,6 +55,28 @@ class LpilPlugin extends Plugin {
     // Set default filename, so long as time and exercise have been set
     if (exercise) {
       const path = `exercise/log/circuits/${readableTimestamp(time)} ${exercise}.md`;
+      if (file.path !== path) {
+        await this.app.fileManager.renameFile(file, path);
+      }
+    }
+  }
+
+  async onBikeRideChanged(file) {
+    let start;
+    let from;
+    let to;
+
+    // Set defaults frontmatter
+    await this.app.fileManager.processFrontMatter(file, (frontmatter) => {
+      frontmatter.start ||= localTimestamp(new Date());
+      start = new Date(frontmatter.start);
+      from = frontmatter.from;
+      to = frontmatter.to;
+    });
+
+    // Set default filename, so long as time and exercise have been set
+    if (from && to) {
+      const path = `exercise/log/bike rides/${readableTimestamp(start)} ${from} to ${to}.md`;
       if (file.path !== path) {
         await this.app.fileManager.renameFile(file, path);
       }
