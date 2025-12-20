8e38ee19b94e88bda91bed0b79f601867c0a53dc
Louis Pilfold <louis@lpil.uk>
2025-12-20 09:56:44 +0000

    Remove luasnip

--- dotfiles/home/.config/nvim/init.lua
+++ dotfiles/home/.config/nvim/init.lua
@@ -499,32 +499,6 @@ require("lazy").setup({
     event = "VimEnter",
     version = "1.*",
     dependencies = {
-      -- Snippet Engine
-      {
-        "L3MON4D3/LuaSnip",
-        version = "2.*",
-        build = (function()
-          -- Build Step is needed for regex support in snippets.
-          -- This step is not supported in many windows environments.
-          -- Remove the below condition to re-enable on windows.
-          if vim.fn.has("win32") == 1 or vim.fn.executable("make") == 0 then
-            return
-          end
-          return "make install_jsregexp"
-        end)(),
-        dependencies = {
-          -- `friendly-snippets` contains a variety of premade snippets.
-          --    See the README about individual language/framework/plugin snippets:
-          --    https://github.com/rafamadriz/friendly-snippets
-          -- {
-          --   'rafamadriz/friendly-snippets',
-          --   config = function()
-          --     require('luasnip.loaders.from_vscode').lazy_load()
-          --   end,
-          -- },
-        },
-        opts = {},
-      },
       "folke/lazydev.nvim",
     },
     --- @module 'blink.cmp'
@@ -576,8 +550,6 @@ require("lazy").setup({
           lazydev = { module = "lazydev.integrations.blink", score_offset = 100 },
         },
       },
-
-      snippets = { preset = "luasnip" },
 
       -- Blink.cmp includes an optional, recommended rust fuzzy matcher,
       -- which automatically downloads a prebuilt binary when enabled.
