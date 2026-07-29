a4a67098e56edc6c6bc7157953ee9f4768bfb741
Louis Pilfold <louis@lpil.uk>
2026-07-29 16:41:46 +0100

    mqttui script

---
+++ home/slowbro/bin/mqttui.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+#
+# Run mqttui, a TUI for viewing mqtt traffic
+#
+
+set -eu
+
+. ./src/secrets.env
+export MQTTUI_PASSWORD="$MOSQUITTO_LOUIS_PASSWORD"
+
+mqttui --broker mqtt://slowbro --username louis
+
+
